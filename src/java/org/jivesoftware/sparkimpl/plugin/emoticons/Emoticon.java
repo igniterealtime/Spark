928298092d9d7d92cc96969603a4222d441bbadc
Derek DeMoro <derek@jivesoftware.com>
2006-11-07 15:22:58 +0000

    Work on Emoticon Theme Packs including Spark default theme pack.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@5973 b35dd754-fafc-0310-a699-88a17e54d16e

---
+++ src/java/org/jivesoftware/sparkimpl/plugin/emoticons/Emoticon.java
@@ -0,0 +1,77 @@
+/**
+ * $Revision: $
+ * $Date: $
+ *
+ * Copyright (C) 2006 Jive Software. All rights reserved.
+ *
+ * This software is published under the terms of the GNU Lesser Public License (LGPL),
+ * a copy of which is included in this distribution.
+ */
+
+package org.jivesoftware.sparkimpl.plugin.emoticons;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a single emoticon.
+ *
+ * @author Derek DeMoro
+ */
+public class Emoticon {
+
+    private String imageName;
+    private String emoticonName;
+
+    private List<String> equivalants = new ArrayList<String>();
+
+
+    /**
+     * Creates a single Emoticon entry.
+     *
+     * @param nameOfImage  the name of the image that represents this emoticon (ex. smile.gif)
+     * @param emoticonName the name of this emoticon
+     * @param equivalants  all string representations of this emoticon.
+     */
+    public Emoticon(String nameOfImage, String emoticonName, List<String> equivalants) {
+        this.imageName = nameOfImage;
+        this.emoticonName = emoticonName;
+
+        this.equivalants = equivalants;
+    }
+
+    /**
+     * Return the name of the image.
+     *
+     * @return image name.
+     */
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    /**
+     * Returns the name of this emoticon.
+     *
+     * @return name of emoticon.
+     */
+    public String getEmoticonName() {
+        return emoticonName;
+    }
+
+    public void setEmoticonName(String emoticonName) {
+        this.emoticonName = emoticonName;
+    }
+
+    /**
+     * Returns all text equivilants of this emoticon.
+     *
+     * @return list of all text equivilants.
+     */
+    public List<String> getEquivalants() {
+        return equivalants;
+    }
+}
--- src/java/org/jivesoftware/sparkimpl/plugin/emoticons/EmoticonManager.java
+++ src/java/org/jivesoftware/sparkimpl/plugin/emoticons/EmoticonManager.java
@@ -10,13 +10,24 @@
 
 package org.jivesoftware.sparkimpl.plugin.emoticons;
 
-import org.jivesoftware.Spark;
-import org.jivesoftware.spark.plugin.PluginClassLoader;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+import org.jivesoftware.spark.util.log.Log;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
- * Responsible for the handling of all Emoticon packs.
+ * Responsible for the handling of all Emoticon packs. Using the EmoticonManager, you can specify
+ * any defined Emoticon Pack, retrieve any emoticon based on its text equivalant, and retrieve its
+ * associated image url.
  *
  * @author Derek DeMoro
  */
@@ -25,13 +36,8 @@ public class EmoticonManager {
     private static EmoticonManager singleton;
     private static final Object LOCK = new Object();
 
-    /**
-     * The root Plugins Directory.
-     */
-    public static File EMOTICON_DIRECTORY = new File(Spark.getBinDirectory().getParent(), "emoticons").getAbsoluteFile();
-
-
-
+    private List<Emoticon> emoticons = new ArrayList<Emoticon>();
+    private File emoticonDirectory;
 
     /**
      * Returns the singleton instance of <CODE>EmoticonManager</CODE>,
@@ -55,5 +61,107 @@ public class EmoticonManager {
 
     private EmoticonManager() {
 
+    }
+
+    /**
+     * Loads an emoticon set.
+     *
+     * @param emoticonSet the directory of the emoticon set to load.
+     */
+    public void loadEmoticonSet(File emoticonSet) {
+        // Set the current emoticon directory.
+        this.emoticonDirectory = emoticonSet;
+
+        final File plist = new File(emoticonSet, "Emoticons.plist");
+        SAXReader saxReader = new SAXReader();
+        Document emoticonFile = null;
+        try {
+            emoticonFile = saxReader.read(plist);
+        }
+        catch (DocumentException e) {
+            Log.error(e);
+        }
+
+        Node root = emoticonFile.selectSingleNode("/plist/dict/dict");
+
+        List keyList = root.selectNodes("key");
+        List dictonaryList = root.selectNodes("dict");
+
+        Iterator keys = keyList.iterator();
+        Iterator dicts = dictonaryList.iterator();
+
+        while (keys.hasNext()) {
+            Element keyEntry = (Element)keys.next();
+            String key = keyEntry.getText();
+
+            Element dict = (Element)dicts.next();
+            String name = dict.selectSingleNode("string").getText();
+
+            // Load equivilants
+            final List<String> equivs = new ArrayList<String>();
+            final List equivilants = dict.selectNodes("array/string");
+            Iterator iter = equivilants.iterator();
+            while (iter.hasNext()) {
+                Element equivilant = (Element)iter.next();
+                String equivilantString = equivilant.getText();
+                equivs.add(equivilantString);
+            }
+
+            final Emoticon emoticon = new Emoticon(key, name, equivs);
+            emoticons.add(emoticon);
+        }
+    }
+
+    /**
+     * Retrieve the URL to an emoticon.
+     *
+     * @param emoticon the emoticon.
+     * @return the URL of the image.
+     */
+    public URL getEmoticonURL(Emoticon emoticon) {
+        final String imageName = emoticon.getImageName();
+
+        File file = new File(emoticonDirectory, imageName);
+        try {
+            return file.toURL();
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Retrieves the associated key emoticon.
+     *
+     * @param key the key.
+     * @return the emoticon.
+     */
+    public Emoticon getEmoticon(String key) {
+        for (Emoticon emoticon : emoticons) {
+            for (String string : emoticon.getEquivalants()) {
+                if (key.equals(string)) {
+                    return emoticon;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Test Bed
+     *
+     * @param args the args man.
+     */
+    public static void main(String args[]) {
+        final File emoticonSet = new File("C:\\adium\\Adiumy.AdiumEmoticonset");
+
+        EmoticonManager manager = EmoticonManager.getInstance();
+        manager.loadEmoticonSet(emoticonSet);
+
+        Emoticon emoticon = manager.getEmoticon(":)");
+        System.out.println(emoticon);
+        System.out.println(manager.getEmoticonURL(emoticon));
     }
 }
