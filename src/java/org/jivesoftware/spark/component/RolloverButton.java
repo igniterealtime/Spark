0bf68c4a6b0dc3841b2629ff01456f55a645b6ab
Derek DeMoro <derek@jivesoftware.com>
2006-12-12 20:33:01 +0000

    Update phone container.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@6379 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/spark/component/RolloverButton.java
+++ src/java/org/jivesoftware/spark/component/RolloverButton.java
@@ -10,39 +10,22 @@
 
 package org.jivesoftware.spark.component;
 
-import org.jivesoftware.resource.Default;
-import org.jivesoftware.resource.SparkRes;
+import org.jivesoftware.Spark;
 
-import javax.swing.BorderFactory;
+import javax.swing.Action;
 import javax.swing.Icon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.JButton;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Image;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.geom.AffineTransform;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Button UI for handling of rollover buttons.
  *
  * @author Derek DeMoro
  */
-public class RolloverButton extends JPanel {
-
-    private JLabel innerLabel = new JLabel();
-
-    private List<ActionListener> listeners = new ArrayList<ActionListener>();
+public class RolloverButton extends JButton {
 
     /**
      * Create a new RolloverButton.
@@ -52,10 +35,14 @@ public class RolloverButton extends JPanel {
     }
 
     public RolloverButton(String text) {
-        innerLabel.setText(text);
+        super(text);
         decorate();
     }
 
+    public RolloverButton(Action action) {
+        super(action);
+        decorate();
+    }
 
     /**
      * Create a new RolloverButton.
@@ -63,12 +50,8 @@ public class RolloverButton extends JPanel {
      * @param icon the icon to use on the button.
      */
     public RolloverButton(Icon icon) {
-        innerLabel.setIcon(icon);
+        super(icon);
         decorate();
-    }
-
-    public void setIcon(Icon icon) {
-        innerLabel.setIcon(icon);
     }
 
     /**
@@ -78,113 +61,39 @@ public class RolloverButton extends JPanel {
      * @param icon the button icon.
      */
     public RolloverButton(String text, Icon icon) {
-        setText(text);
-        setIcon(icon);
+        super(text, icon);
         decorate();
     }
 
-    public void addActionListener(ActionListener listener) {
-        listeners.add(listener);
-    }
-
-    public void removeActionListener(ActionListener listener) {
-        listeners.remove(listener);
-    }
-
-    public void setText(String text) {
-        innerLabel.setText(text);
-    }
-
-    public JLabel getInnerLabel() {
-        return innerLabel;
-    }
-
-    public void setEnabled(boolean enabled) {
-        innerLabel.setEnabled(enabled);
-        setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, SparkRes.getImageIcon(SparkRes.BLANK_IMAGE)));
-    }
-
-    public void setMnemonic(int mnemonic) {
-        innerLabel.setDisplayedMnemonic(mnemonic);
-    }
-
-    public void setVerticalTextPosition(int pos) {
-        innerLabel.setVerticalTextPosition(pos);
-    }
-
-    public void setHorizontalTextPosition(int pos) {
-        innerLabel.setHorizontalTextPosition(pos);
-    }
-
-    public void addMouseListener(MouseListener listener) {
-        innerLabel.addMouseListener(listener);
-    }
 
     /**
      * Decorates the button with the approriate UI configurations.
      */
     private void decorate() {
-        setLayout(new GridBagLayout());
-        add(innerLabel, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(1, 3, 1, 3), 0, 0));
-        setOpaque(false);
-        setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, SparkRes.getImageIcon(SparkRes.BLANK_IMAGE)));
+        setBorderPainted(false);
+        setOpaque(true);
 
-        innerLabel.addMouseListener(new MouseListener() {
-            public void mouseClicked(MouseEvent e) {
-            }
+        setContentAreaFilled(false);
+        setMargin(new Insets(1, 1, 1, 1));
 
-            public void mousePressed(MouseEvent e) {
-                if (!innerLabel.isEnabled()) {
-                    return;
-                }
-                setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.lightGray));
-                setOpaque(true);
-
-            }
-
-            public void mouseReleased(MouseEvent e) {
-                if (!innerLabel.isEnabled()) {
-                    return;
-                }
-                setOpaque(false);
-
-                for (ActionListener listener : listeners) {
-                    listener.actionPerformed(getActionEvent());
-                }
-            }
-
+        addMouseListener(new MouseAdapter() {
             public void mouseEntered(MouseEvent e) {
-                if (!innerLabel.isEnabled()) {
-                    return;
+                if (isEnabled()) {
+                    setBorderPainted(true);
+
+                    // Handle background border on mac.
+                    if (!Spark.isMac()) {
+                        setContentAreaFilled(true);
+                    }
                 }
-                setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.lightGray));
             }
 
             public void mouseExited(MouseEvent e) {
-                if (!innerLabel.isEnabled()) {
-                    return;
-                }
-                setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, SparkRes.getImageIcon(SparkRes.BLANK_IMAGE)));
+                setBorderPainted(false);
+                setContentAreaFilled(false);
             }
         });
-    }
 
-    public ActionEvent getActionEvent() {
-        ActionEvent e = new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "");
-        return e;
-    }
-
-
-    public void paintComponent(Graphics g) {
-        if (!isOpaque()) {
-            super.paintComponent(g);
-            return;
-        }
-        final Image backgroundImage = Default.getImageIcon(Default.SECONDARY_BACKGROUND_IMAGE).getImage();
-        double scaleX = getWidth() / (double)backgroundImage.getWidth(null);
-        double scaleY = getHeight() / (double)backgroundImage.getHeight(null);
-        AffineTransform xform = AffineTransform.getScaleInstance(scaleX, scaleY);
-        ((Graphics2D)g).drawImage(backgroundImage, xform, this);
     }
 
 
