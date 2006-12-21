6c316f6043cef272aa68c3e1715f41134b7f797e
Derek DeMoro <derek@jivesoftware.com>
2006-12-21 16:12:37 +0000

    Increased the default height of the send field.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@6464 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/spark/ui/ChatRoom.java
+++ src/java/org/jivesoftware/spark/ui/ChatRoom.java
@@ -211,7 +211,7 @@ public abstract class ChatRoom extends BackgroundPanel implements ActionListener
         bottomPanel.setOpaque(false);
         splitPane.setOpaque(false);
         bottomPanel.setLayout(new GridBagLayout());
-        bottomPanel.add(chatAreaButton, new GridBagConstraints(0, 1, 5, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+        bottomPanel.add(chatAreaButton, new GridBagConstraints(0, 1, 5, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 10));
         bottomPanel.add(editorBar, new GridBagConstraints(0, 0, 5, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
         verticalSplit.setOpaque(false);
 
