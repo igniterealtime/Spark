ef439a992b5d09fec873047f8229e5bb588b1ca5
Derek DeMoro <derek@jivesoftware.com>
2007-03-19 22:39:02 +0000

    Allow for remote debugging in spark.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@7601 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/resources/startup.bat
+++ src/resources/startup.bat
@@ -1,1 +1,9 @@
+:run
+if "%1" == "-debug" goto debug
 java -Dappdir=.. -cp ../lib/windows/jmf.jar;../lib/startup.jar;../lib/windows/jdic.jar;../resources;../lib/windows; org.jivesoftware.launcher.Startup
+goto end
+
+:debug
+start "Spark" "%JAVA_HOME%\bin\java" -Xdebug -Xint -server -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -Dappdir=.. -cp ../lib/windows/jmf.jar;../lib/startup.jar;../lib/windows/jdic.jar;../resources;../lib/windows; org.jivesoftware.launcher.Startup
+goto end
+:end
