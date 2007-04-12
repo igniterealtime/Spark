7b2395b660035e79bde027d6ef3d1b93d61dd582
Francisco Vives <francisco.vives@clarolab.com>
2007-04-12 21:42:29 +0000

    SPARK-626 Jars missing from the classpath in Linux
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@8003 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/resources/startup.sh
+++ src/resources/startup.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-:mac
-if "%1" == "-linux" goto linux
-java -Dappdir=.. -cp ../lib/mac/JavaSoundStream.fix.jar:../lib/mac/jmf.jar:../lib/startup.jar:../lib/windows/jdic.jar:../resources org.jivesoftware.launcher.Startup
-goto end
-
-:linux
 java -Dappdir=.. -cp ../lib/linux/jmf.jar:../lib/startup.jar:../lib/linux/jdic.jar:../resources org.jivesoftware.launcher.Startup
-goto end
-:end
