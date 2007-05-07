97ed1bcd898cd9efe297f7522e92870d358e492f
Derek DeMoro <derek@jivesoftware.com>
2007-05-07 04:44:36 +0000

    Fixed issue with updating from beta.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@8196 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/sparkimpl/updater/CheckUpdates.java
+++ src/java/org/jivesoftware/sparkimpl/updater/CheckUpdates.java
@@ -40,13 +40,6 @@ import org.jivesoftware.sparkimpl.settings.JiveInfo;
 import org.jivesoftware.sparkimpl.settings.local.LocalPreferences;
 import org.jivesoftware.sparkimpl.settings.local.SettingsManager;
 
-import javax.swing.JEditorPane;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JProgressBar;
-import javax.swing.JScrollPane;
-import javax.swing.text.html.HTMLEditorKit;
-
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -62,6 +55,13 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.TimerTask;
+
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.text.html.HTMLEditorKit;
 
 public class CheckUpdates {
     private String mainUpdateURL;
@@ -413,8 +413,8 @@ public class CheckUpdates {
 
             ConfirmDialog confirm = new ConfirmDialog();
             confirm.showConfirmDialog(SparkManager.getMainWindow(), Res.getString("title.new.version.available"),
-                    Res.getString("message.new.spark.available", filename), Res.getString("yes"), Res.getString("no"),
-                    null);
+                Res.getString("message.new.spark.available", filename), Res.getString("yes"), Res.getString("no"),
+                null);
             confirm.setDialogSize(400, 300);
             confirm.setConfirmListener(new ConfirmListener() {
                 public void yesOption() {
@@ -503,7 +503,8 @@ public class CheckUpdates {
         }
         else if (versionTwoBetaOrAlpha) {
             String versionTwo = getVersion(secondVersion);
-            return (firstVersion.compareTo(versionTwo) >= 1);
+            int result = firstVersion.compareTo(versionTwo);
+            return result >= 0;
         }
 
 
@@ -590,8 +591,8 @@ public class CheckUpdates {
     private void promptForInstallation(final File downloadedFile, String title, String message) {
         ConfirmDialog confirm = new ConfirmDialog();
         confirm.showConfirmDialog(SparkManager.getMainWindow(), title,
-                message, Res.getString("yes"), Res.getString("no"),
-                null);
+            message, Res.getString("yes"), Res.getString("no"),
+            null);
         confirm.setConfirmListener(new ConfirmListener() {
             public void yesOption() {
                 try {
