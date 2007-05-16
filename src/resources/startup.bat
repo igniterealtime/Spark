0e2b47e5339d37a75e1a1fe7b37231005a7b33f3
Francisco Vives <francisco.vives@clarolab.com>
2007-05-16 17:28:55 +0000

    Add debug options
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@8331 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/resources/startup.bat
+++ src/resources/startup.bat
@@ -4,6 +4,6 @@ java -Dappdir=.. -cp ../lib/windows/jmf.jar;../lib/startup.jar;../lib/windows/jd
 goto end
 
 :debug
-start "Spark" "%JAVA_HOME%\bin\java" -Xdebug -Xint -server -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -Dappdir=.. -cp ../lib/windows/jmf.jar;../lib/startup.jar;../lib/windows/jdic.jar;../resources;../lib/windows; org.jivesoftware.launcher.Startup
+start "Spark" "%JAVA_HOME%\bin\java" -Ddebugger=true -Ddebug.mode=true -XX:+HeapDumpOnOutOfMemoryError -Xdebug -Xint -server -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -Dappdir=.. -cp ../lib/windows/jmf.jar;../lib/startup.jar;../lib/windows/jdic.jar;../resources;../lib/windows; org.jivesoftware.launcher.Startup
 goto end
 :end
