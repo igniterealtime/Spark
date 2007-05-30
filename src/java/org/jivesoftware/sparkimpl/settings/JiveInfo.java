6f3b816e59d32fcb2ad56646f71e13b1a3a0eef7
Derek DeMoro <derek@jivesoftware.com>
2007-05-30 21:42:33 +0000

    Update
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@8411 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/sparkimpl/settings/JiveInfo.java
+++ src/java/org/jivesoftware/sparkimpl/settings/JiveInfo.java
@@ -17,11 +17,11 @@ public class JiveInfo {
     }
 
     public static String getVersion() {
-        return "2.5.3.beta2";
+        return "2.5.4.beta1";
     }
 
     public static String getBuildNumber() {
-        return "2.5.3.beta2";
+        return "2.5.4.beta1";
     }
 
     public static String getOS() {
