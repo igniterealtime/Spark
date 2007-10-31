f2424f6b4744d17e550a50874ad7ae5e02fcb5df
Gaston Dombiak <gaston@jivesoftware.com>
2007-10-31 16:41:53 +0000

    Updated for 2.5.8 release.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@9402 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/sparkimpl/settings/JiveInfo.java
+++ src/java/org/jivesoftware/sparkimpl/settings/JiveInfo.java
@@ -17,11 +17,11 @@ public class JiveInfo {
     }
 
     public static String getVersion() {
-        return "2.5.8.beta1";
+        return "2.5.8";
     }
 
     public static String getBuildNumber() {
-        return "2.5.8.beta1";
+        return "2.5.8";
     }
 
     public static String getOS() {
