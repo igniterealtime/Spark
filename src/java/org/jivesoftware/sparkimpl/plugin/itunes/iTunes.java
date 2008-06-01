a4516253cf32540a1e7dafc56ec69dc704e79aec
Andrew Seymour <commit@andrewseymour.co.uk>
2008-06-01 08:53:29 +0000

    Essentially, just a small update for now, added new Libraries and started the writing for the iTunes plugin, the libraries are there to allow COM communications. Both DLL's are there with a 32bit (x86) and 64bit (x64) version.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@10465 b35dd754-fafc-0310-a699-88a17e54d16e

---
+++ src/java/org/jivesoftware/sparkimpl/plugin/itunes/iTunes.java
@@ -0,0 +1,16 @@
+/*
+ * iTunes.java
+ *
+ * Created on 28 May 2008, 00:36
+ */
+
+package org.jivesoftware.sparkimpl.plugin.itunes;
+//package org.jivesoftware.spark.util;
+
+import com.jacob.com.ComThread;
+import com.jacob.com.Dispatch;
+import com.jacob.activeX.ActiveXComponent;
+
+public class iTunes {
+    /* the beginings of the iTunes plugin */
+}
