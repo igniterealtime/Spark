c536bf451464c515249a904655d7e44f972729cf
Michael Will <michael.will@willwebhosting.de>
2009-03-13 19:43:15 +0000

    Apply patch from niklas SPARK-978_BuzzDelayShouldNotReset.patch
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@10977 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/sparkimpl/plugin/alerts/BuzzRoomDecorator.java
+++ src/java/org/jivesoftware/sparkimpl/plugin/alerts/BuzzRoomDecorator.java
@@ -10,6 +10,13 @@
 
 package org.jivesoftware.sparkimpl.plugin.alerts;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.TimerTask;
+
+import javax.swing.JLabel;
+
 import org.jivesoftware.resource.Res;
 import org.jivesoftware.resource.SparkRes;
 import org.jivesoftware.smack.packet.Message;
@@ -17,41 +24,55 @@ import org.jivesoftware.spark.ChatManager;
 import org.jivesoftware.spark.SparkManager;
 import org.jivesoftware.spark.component.RolloverButton;
 import org.jivesoftware.spark.ui.ChatRoom;
-import org.jivesoftware.spark.ui.ChatRoomClosingListener;
 import org.jivesoftware.spark.ui.rooms.ChatRoomImpl;
 import org.jivesoftware.spark.util.SwingTimerTask;
 import org.jivesoftware.spark.util.TaskEngine;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.TimerTask;
-
-import javax.swing.JLabel;
 
 /**
  * Adds a simple buzz operation button the each newly created ChatRoom.
  *
  * @author Derek DeMoro
  */
-public class BuzzRoomDecorator implements ActionListener, ChatRoomClosingListener {
+public class BuzzRoomDecorator implements ActionListener {
 
     private ChatRoom chatRoom;
     private RolloverButton buzzButton;
-
+    private static ArrayList<BuzzRoomDecorator> objects = new ArrayList<BuzzRoomDecorator>();
+    private String jid;
+    
     public BuzzRoomDecorator(ChatRoom chatRoom) {
         this.chatRoom = chatRoom;
-
-        buzzButton = new RolloverButton(SparkRes.getImageIcon(SparkRes.BUZZ_IMAGE));
-        buzzButton.setToolTipText(Res.getString("message.buzz.alert.notification"));
-        buzzButton.addActionListener(this);
-
-        final JLabel dividerLabel = new JLabel(SparkRes.getImageIcon("DIVIDER_IMAGE"));
-        chatRoom.getEditorBar().add(dividerLabel);
-        chatRoom.getEditorBar().add(buzzButton);
-
-        chatRoom.addClosingListener(this);
+        jid = ((ChatRoomImpl)chatRoom).getParticipantJID();
+        boolean added = false;
+        
+        for(BuzzRoomDecorator buzz : objects)
+        {
+      	  if(buzz.jid == ((ChatRoomImpl)chatRoom).getParticipantJID())
+      	  {
+      		  addBuzzButton(buzz);
+      		  added = true;
+      	  }
+        }
+        
+        if(!added)
+        {
+	        buzzButton = new RolloverButton(SparkRes.getImageIcon(SparkRes.BUZZ_IMAGE));
+	        buzzButton.setToolTipText(Res.getString("message.buzz.alert.notification"));
+	        buzzButton.addActionListener(this);
+	
+	        final JLabel dividerLabel = new JLabel(SparkRes.getImageIcon("DIVIDER_IMAGE"));
+	        chatRoom.getEditorBar().add(dividerLabel);
+	        chatRoom.getEditorBar().add(buzzButton);
+	        objects.add(this);
+        }
     }
 
+    public void addBuzzButton(BuzzRoomDecorator buzzer)
+    {
+    	final JLabel dividerLabel = new JLabel(SparkRes.getImageIcon("DIVIDER_IMAGE"));
+    	chatRoom.getEditorBar().add(dividerLabel);
+    	chatRoom.getEditorBar().add(buzzer.buzzButton);
+    }
 
     public void actionPerformed(ActionEvent e) {
         final String jid = ((ChatRoomImpl)chatRoom).getParticipantJID();
@@ -71,11 +92,5 @@ public class BuzzRoomDecorator implements ActionListener, ChatRoomClosingListene
         };
 
         TaskEngine.getInstance().schedule(enableTask, 30000);
-    }
-
-
-    public void closing() {
-        chatRoom.removeClosingListener(this);
-        buzzButton.removeActionListener(this);
     }
 }
