7c75636f72f0fb4829ba062299d3d58f100e93bd
Michael Will <michael.will@willwebhosting.de>
2009-10-27 08:36:19 +0000

    add jdom to the startup.sh and startup.bat
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@11332 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/resources/startup.sh
+++ src/resources/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-java -Dappdir=.. -cp ../lib/log4j.jar:../lib/lti-civil.jar:../lib/fmj.jar:../lib/startup.jar:../lib/linux/jdic.jar:../resources org.jivesoftware.launcher.Startup
+java -Dappdir=.. -cp ../lib/jdom.jar:../lib/log4j.jar:../lib/lti-civil.jar:../lib/fmj.jar:../lib/startup.jar:../lib/linux/jdic.jar:../resources org.jivesoftware.launcher.Startup
 
 java -Ddebugger=true -Ddebug.mode=true -XX:+HeapDumpOnOutOfMemoryError -Xdebug -Xint -server -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -Dappdir=.. -cp ../lib/log4j.jar:../lib/lti-civil.jar:../lib/fmj.jar:../lib/startup.jar:../lib/linux/jdic.jar:../resources org.jivesoftware.launcher.Startup
 
