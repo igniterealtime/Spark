dc652f68b7ef5271dcde5e5f7a95503f7b6f5337
Michael Will <michael.will@willwebhosting.de>
2010-05-28 07:08:55 +0000

    SPARK-1139 - Improved linux startup.sh
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@11702 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/resources/startup.sh
+++ src/resources/startup.sh
@@ -1,5 +1,57 @@
 #!/bin/bash
-java -Dappdir=.. -cp ../lib/jdom.jar:../lib/log4j.jar:../lib/lti-civil.jar:../lib/fmj.jar:../lib/startup.jar:../lib/linux/jdic.jar:../resources org.jivesoftware.launcher.Startup
 
-java -Ddebugger=true -Ddebug.mode=true -XX:+HeapDumpOnOutOfMemoryError -Xdebug -Xint -server -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -Dappdir=.. -cp ../lib/log4j.jar:../lib/lti-civil.jar:../lib/fmj.jar:../lib/startup.jar:../lib/linux/jdic.jar:../resources org.jivesoftware.launcher.Startup
+scriptdir="`dirname ${0}`";
+cd "${scriptdir}/..";
+wd="`pwd`";
+libdir="${wd}/lib";
+
+classpath="\
+${libdir}/jdom.jar:\
+${libdir}/log4j.jar:\
+${libdir}/lti-civil.jar:\
+${libdir}/fmj.jar:\
+${libdir}/startup.jar:\
+${libdir}/linux/jdic.jar:\
+${wd}/resources\
+";
+
+# add java library path folder depending on architecture
+case "`uname -m`" in
+  "x86_64")
+  javalibrarypath="-Djava.library.path=${libdir}/linux64";
+  ;;
+   
+  "*")
+  javalibrarypath="-Djava.library.path=${libdir}/linux"; 
+   ;;
+esac;
+
+echo "using classpath: ${classpath}";
+
+mainclass="org.jivesoftware.launcher.Startup";
+
+if [ "${1}" = "-debug" ]; then
+
+java -Ddebugger=true \
+-Ddebug.mode=true \
+-XX:+HeapDumpOnOutOfMemoryError \
+-Xdebug \
+-Xint \
+-server \
+-Xnoagent \
+-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 \
+"-Dappdir=${wd}" \
+"${javalibrarypath}" \
+-cp "${classpath}" \
+${mainclass}
+
+else
+
+  java \
+"-Dappdir=${wd}" \
+"${javalibrarypath}" \
+-cp "${classpath}" \
+${mainclass}
+
+fi;
 
