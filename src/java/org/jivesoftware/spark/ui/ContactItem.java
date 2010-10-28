e3428dc6ce2df49f45b9f793a6d411d7971b2bf2
Michael Will <michael.will@willwebhosting.de>
2010-10-28 07:16:33 +0000

    SPARK-572 - Custom status messages under "On Phone" status now show as phone icon.
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@11977 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/spark/ui/ContactItem.java
+++ src/java/org/jivesoftware/spark/ui/ContactItem.java
@@ -479,7 +479,7 @@ public class ContactItem extends JPanel {
             setStatus(status);
         }
 
-        if (status != null && status.toLowerCase().indexOf("phone") != -1 && presence.getMode() == Presence.Mode.away) {
+        if (PresenceManager.isOnPhone(presence)) {
             statusIcon = SparkRes.getImageIcon(SparkRes.ON_PHONE_IMAGE);
             setIcon(statusIcon);  
         }
