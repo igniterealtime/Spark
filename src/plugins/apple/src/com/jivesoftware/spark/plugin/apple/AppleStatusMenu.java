ca795db0541617009785d0561487109f08f32ea3
Michael Will <michael.will@willwebhosting.de>
2010-11-04 08:30:45 +0000

    Change to Apache 2.0
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@11988 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/plugins/apple/src/com/jivesoftware/spark/plugin/apple/AppleStatusMenu.java
+++ src/plugins/apple/src/com/jivesoftware/spark/plugin/apple/AppleStatusMenu.java
@@ -1,7 +1,21 @@
 /**
- * Copyright (C) 1999-2004 Jive Software. All rights reserved.
+ * $RCSfile: ,v $
+ * $Revision: $
+ * $Date: $
+ * 
+ * Copyright (C) 2004-2010 Jive Software. All rights reserved.
  *
- * This software is the proprietary information of Jive Software. Use is subject to license terms.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.jivesoftware.spark.plugin.apple;
 
