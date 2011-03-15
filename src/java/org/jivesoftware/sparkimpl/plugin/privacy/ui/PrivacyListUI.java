ce54994e69aef9e069b38411be1b50749b652327
Wolf Posdorfer <mordredd666@gmail.com>
2011-03-15 08:46:22 +0000

    should be package org.jivesoftware.sparkimpl.plugin.privacy.ui not java.org....
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@12117 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/sparkimpl/plugin/privacy/ui/PrivacyListUI.java
+++ src/java/org/jivesoftware/sparkimpl/plugin/privacy/ui/PrivacyListUI.java
@@ -17,7 +17,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package java.org.jivesoftware.sparkimpl.plugin.privacy.ui;
+package org.jivesoftware.sparkimpl.plugin.privacy.ui;
 
 import javax.swing.JPanel;
 
