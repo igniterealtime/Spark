f73cd6d4351c5d4c808b35a8572c3ce9c9dc7399
Wolf Posdorfer <mordredd666@gmail.com>
2011-03-23 18:36:13 +0000

    SPARK-1220 and SPARK-1124 We're on a 64bit platform, running 64bit Java, and you claim you cant load a 64bit.dll and rather want a 32bit dll....  go ahead, take it then...
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@12169 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/plugins/flashing/src/java/org/jivesoftware/spark/plugin/flashing/FlashWindow.java
+++ src/plugins/flashing/src/java/org/jivesoftware/spark/plugin/flashing/FlashWindow.java
@@ -36,14 +36,24 @@ public class FlashWindow {
 
 	static {
 	    
-	boolean is64bit = Integer.parseInt(System.getProperty("sun.arch.data.model")) == 64;
+	boolean is64bit = System.getProperty("sun.arch.data.model").equals("64");
 	String arch = "";
 	if (is64bit) {
 	    arch = "64";
 	}
-		System.load(PluginManager.PLUGINS_DIRECTORY + File.separator
-				+ "flashing" + File.separator + "lib" + File.separator
-				+ "FlashWindow"+arch+".dll");
+	
+	try {
+	    System.load(PluginManager.PLUGINS_DIRECTORY + File.separator + "flashing" + File.separator + "lib"
+		    + File.separator + "FlashWindow" + arch + ".dll");
+	} catch (UnsatisfiedLinkError e) {
+	    // So, we are on 64bit using 64bit java and you rather wand a 32bit.dll ?? suuuureeee....
+	    if (e.getMessage().contains("Can't load AMD 64-bit .dll on a IA 32-bit platform")) {
+		System.load(PluginManager.PLUGINS_DIRECTORY + File.separator + "flashing" + File.separator + "lib"
+			+ File.separator + "FlashWindow.dll");
+	    } else {
+		e.printStackTrace();
+	    }
+	}
 		// System.load("C:\\PATH\FlashWindow"+s+".dll");
 	}
 
