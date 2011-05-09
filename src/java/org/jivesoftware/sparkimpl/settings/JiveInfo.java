c94eb8b5c165fba369e9212800eb2f9bc0f000ac
Wolf Posdorfer <mordredd666@gmail.com>
2011-05-09 07:03:19 +0000

    2.6.0
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@12344 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/sparkimpl/settings/JiveInfo.java
+++ src/java/org/jivesoftware/sparkimpl/settings/JiveInfo.java
@@ -27,7 +27,7 @@ public class JiveInfo {
     }
 
     public static String getVersion() {
-        return "2.6.0 RC 2";
+        return "2.6.0";
     }
 
     /*
@@ -37,7 +37,7 @@ public class JiveInfo {
      * SVN revision number of the build 
      */
     public static String getBuildNumber() {
-        return "12285";
+        return "12343";
     }
 
     public static String getOS() {
