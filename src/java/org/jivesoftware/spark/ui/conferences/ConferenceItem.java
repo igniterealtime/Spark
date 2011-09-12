9910b689885e74799e9996cd9c7e3259b95845fa
mirceac <mirceac@b35dd754-fafc-0310-a699-88a17e54d16e>
2011-09-12 08:45:45 +0000

    SPARK-1403: Add eZuce copyright for the new classes
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@12591 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/spark/ui/conferences/ConferenceItem.java
+++ src/java/org/jivesoftware/spark/ui/conferences/ConferenceItem.java
@@ -1,3 +1,22 @@
+/**
+ * $RCSfile: ,v $
+ * $Revision: $
+ * $Date: $
+ *  
+ * Copyright (C) 2011 eZuce Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jivesoftware.spark.ui.conferences;
 
 import org.jivesoftware.smackx.bookmark.BookmarkedConference;
