16fffc3f551fda112234fdc8ffa3fbf5aba44003
Tim Jentz <timjentz@b35dd754-fafc-0310-a699-88a17e54d16e>
2012-05-18 08:44:44 +0000

    SPARK-1494: Spark's static logger, accessing static variables which are initialized in a non-static way. This may cause nullpointer exceptions on startup. Change the Logger to singleton to make sure variables are initialized when accessed
    git-svn-id: http://svn.igniterealtime.org/svn/repos/spark/trunk@13063 b35dd754-fafc-0310-a699-88a17e54d16e

--- src/java/org/jivesoftware/spark/util/log/Log.java
+++ src/java/org/jivesoftware/spark/util/log/Log.java
@@ -30,117 +30,128 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 /**
- * Creates and writes out messages to a a log file. This should be used for all error handling within
- * the Agent application.
+ * Creates and writes out messages to a a log file. This should be used for all
+ * error handling within the Agent application.
  */
 public class Log {
-    private static java.util.logging.Logger ERROR_LOGGER;
+	private java.util.logging.Logger ERROR_LOGGER;
+	private Logger WARNING_LOGGER;
 
-    private static Logger WARNING_LOGGER;
+	private volatile static Log singleton = null;
 
-    private Log() {
-        // Do not allow initialization
-    }
+	private static Log getInstance() {
+		if (singleton == null) {
+			synchronized (Log.class) {
+				if (singleton == null) {
+					singleton = new Log();
+				}
+			}
+		}
+		return singleton;
+	}
 
-    static {
-        if (!Spark.getLogDirectory().exists()) {
-            Spark.getLogDirectory().mkdirs();
-        }
+	private Log() {
+		if (!Spark.getLogDirectory().exists()) {
+			Spark.getLogDirectory().mkdirs();
+		}
+		ERROR_LOGGER = java.util.logging.Logger.getAnonymousLogger();
+		WARNING_LOGGER = java.util.logging.Logger.getAnonymousLogger();
 
-        File ERROR_LOG_FILE = new File(Spark.getLogDirectory(), "errors.log");
-        File WARNING_LOG_FILE = new File(Spark.getLogDirectory(), "warn.log");
+		File ERROR_LOG_FILE = new File(Spark.getLogDirectory(), "errors.log");
+		File WARNING_LOG_FILE = new File(Spark.getLogDirectory(), "warn.log");
 
+		try {
+			// Create an appending file handler
+			boolean append = true;
+			FileHandler errorHandler = new FileHandler(
+					ERROR_LOG_FILE.getCanonicalPath(), append);
+			errorHandler.setFormatter(new SimpleFormatter());
 
-        try {
-            // Create an appending file handler
-            boolean append = true;
-            FileHandler errorHandler = new FileHandler(ERROR_LOG_FILE.getCanonicalPath(), append);
-            errorHandler.setFormatter(new SimpleFormatter());
+			FileHandler warnHandler = new FileHandler(
+					WARNING_LOG_FILE.getCanonicalPath(), append);
+			warnHandler.setFormatter(new SimpleFormatter());
 
-            FileHandler warnHandler = new FileHandler(WARNING_LOG_FILE.getCanonicalPath(), append);
-            warnHandler.setFormatter(new SimpleFormatter());
+			// Add to the desired logger
+			ERROR_LOGGER.addHandler(errorHandler);
+			WARNING_LOGGER.addHandler(warnHandler);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-            // Add to the desired logger
-            ERROR_LOGGER = java.util.logging.Logger.getAnonymousLogger();
-            ERROR_LOGGER.addHandler(errorHandler);
+	/**
+	 * Adds a handler to the error logger
+	 * 
+	 * @param h the handler to add
+	 */
+	public synchronized static void addErrorHandler(Handler h) {
+		getInstance().ERROR_LOGGER.addHandler(h);
+	}
 
-            WARNING_LOGGER = Logger.getAnonymousLogger();
-            WARNING_LOGGER.addHandler(warnHandler);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    /**
-     * Adds a handler to the error logger
-     * @param h the handler to add
-     */
-    public synchronized static void addErrorHandler(Handler  h){
-    	ERROR_LOGGER.addHandler(h);
-    }
-    
-    /**
-     * Adds a handler to the error logger
-     * @param h the handler to add
-     */
-    public synchronized static void addWarningHandler(Handler  h){
-    	WARNING_LOGGER.addHandler(h);
-    }
-    
-    /**
-     * Logs all error messages to default error logger.
-     *
-     * @param message a message to append to log file.
-     * @param ex      the exception being thrown.
-     */
-    public static void error(String message, Throwable ex) {
-        ERROR_LOGGER.log(Level.SEVERE, message, ex);
-    }
+	/**
+	 * Adds a handler to the error logger
+	 * 
+	 * @param h the handler to add
+	 */
+	public synchronized static void addWarningHandler(Handler h) {
+		getInstance().WARNING_LOGGER.addHandler(h);
+	}
 
-    /**
-     * Logs all error messages to default error logger.
-     *
-     * @param ex the exception being thrown.
-     */
-    public static void error(Throwable ex) {
-        ERROR_LOGGER.log(Level.SEVERE, "", ex);
-    }
+	/**
+	 * Logs all error messages to default error logger.
+	 * 
+	 * @param message
+	 *            a message to append to log file.
+	 * @param ex the exception being thrown.
+	 */
+	public static void error(String message, Throwable ex) {
+		getInstance().ERROR_LOGGER.log(Level.SEVERE, message, ex);
+	}
 
-    /**
-     * Log a warning message to the default logger.
-     *
-     * @param message the message to log.
-     * @param ex      the exception.
-     */
-    public static void warning(String message, Throwable ex) {
-        WARNING_LOGGER.log(Level.WARNING, message, ex);
-    }
+	/**
+	 * Logs all error messages to default error logger.
+	 * 
+	 * @param ex the exception being thrown.
+	 */
+	public static void error(Throwable ex) {
+		getInstance().ERROR_LOGGER.log(Level.SEVERE, "", ex);
+	}
 
-    public static void warning(String message) {
-        WARNING_LOGGER.log(Level.WARNING, message);
-    }
+	/**
+	 * Log a warning message to the default logger.
+	 * 
+	 * @param message the message to log.
+	 * @param ex the exception.
+	 */
+	public static void warning(String message, Throwable ex) {
+		getInstance().WARNING_LOGGER.log(Level.WARNING, message, ex);
+	}
 
-    /**
-     * Logs all error messages to default error logger.
-     *
-     * @param message a message to append to log file.
-     */
-    public static void error(String message) {
-        ERROR_LOGGER.log(Level.SEVERE, message);
-    }
+	public static void warning(String message) {
+		getInstance().WARNING_LOGGER.log(Level.WARNING, message);
+	}
 
-    /**
-     * Logs all messages to standard errout for debugging purposes.
-     * To use, pass in the VM Parameters debug.mode=true.
-     * <p/>
-     * ex. (-Ddebug.mode=true)
-     *
-     * @param message the message to print out.
-     */
-    public static void debug(String message) {
-        if (System.getProperty("debug.mode") != null) {
-            ERROR_LOGGER.info(message);
-        }
-    }
+	/**
+	 * Logs all error messages to default error logger.
+	 * 
+	 * @param message a message to append to log file.
+	 */
+	public static void error(String message) {
+		getInstance().ERROR_LOGGER.log(Level.SEVERE, message);
+	}
+
+	/**
+	 * Logs all messages to standard errout for debugging purposes. To use, pass
+	 * in the VM Parameters debug.mode=true.
+	 * <p/>
+	 * ex. (-Ddebug.mode=true)
+	 * 
+	 * @param message the message to print out.
+	 */
+	public static void debug(String message) {
+		if (System.getProperty("debug.mode") != null) {
+			getInstance().ERROR_LOGGER.info(message);
+		}
+	}
 
 }
