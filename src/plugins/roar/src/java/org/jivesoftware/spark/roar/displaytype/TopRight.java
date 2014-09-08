076532550cf60b299f9b848c014b1478a736c82e
Wolf Posdorfer <9posdorf@informatik.uni-hamburg.de>
2014-09-08 09:29:01 +0200

    https://community.igniterealtime.org/message/242291 make roar stay indefinetly when time is set to 0

--- src/plugins/roar/src/java/org/jivesoftware/spark/roar/displaytype/TopRight.java
+++ src/plugins/roar/src/java/org/jivesoftware/spark/roar/displaytype/TopRight.java
@@ -28,111 +28,106 @@ import org.jivesoftware.spark.ui.ChatRoom;
  */
 public class TopRight implements RoarDisplayType {
 
-    private int _lastusedXpos;
-    private int _lastusedYpos;
-    private Dimension _screensize;
+	private int _lastusedXpos;
+	private int _lastusedYpos;
+	private Dimension _screensize;
 
-    private int _amount;
+	private int _amount;
 
-    private final int WIDTH = RoarPanel.WIDTH;
-    private final int HEIGHT = RoarPanel.HEIGHT;
+	private final int WIDTH = RoarPanel.WIDTH;
+	private final int HEIGHT = RoarPanel.HEIGHT;
 
-    private Action _customaction;
+	private Action _customaction;
 
-    public TopRight() {
-	_screensize = Toolkit.getDefaultToolkit().getScreenSize();
+	public TopRight() {
+		_screensize = Toolkit.getDefaultToolkit().getScreenSize();
 
-	_lastusedXpos = _screensize.width - 5;
-	_lastusedYpos = 5;
-	_amount = 0;
-
-	_customaction = new AbstractAction() {
-	    private static final long serialVersionUID = -7237306342417462544L;
-
-	    @Override
-	    public void actionPerformed(ActionEvent e) {
-		ChatFrame chatFrame = SparkManager.getChatManager()
-			.getChatContainer().getChatFrame();
-		chatFrame.setState(Frame.NORMAL);
-		chatFrame.setVisible(true);
-	    }
-	};
-    }
-
-    @Override
-    public void messageReceived(ChatRoom room, Message message) {
-	RoarProperties props = RoarProperties.getInstance();
-
-	if (props.getShowingPopups()
-		&& (_amount < props.getMaximumPopups() || props
-			.getMaximumPopups() == 0)) {
-
-	    ImageIcon icon = SparkRes.getImageIcon(SparkRes.SPARK_IMAGE_32x32);
-
-	    String nickname = SparkManager.getUserManager().getUserNicknameFromJID(message.getFrom());
-	    if(room.getChatType() == Message.Type.groupchat)
-	    {
-		nickname = StringUtils.parseResource(nickname);
-	    }
-
-        boolean broadcast = message.getProperty("broadcast") != null;
-
-        if ((broadcast || message.getType() == Message.Type.normal
-                || message.getType() == Message.Type.headline) && message.getBody() != null) {
-           nickname = Res.getString("broadcast") + " - " + nickname;
-        }
-
-	    RoarPanel.popupWindow(this, icon, nickname, message.getBody(),
-		    _lastusedXpos, _lastusedYpos, props.getDuration(),
-		    props.getBackgroundColor(), props.getHeaderColor(),
-		    props.getTextColor(), _customaction);
-
-	    ++_amount;
-	    _lastusedYpos += HEIGHT + 5;
-
-	    if (_lastusedYpos >= _screensize.height - 90) {
-		_lastusedXpos -= WIDTH + 5;
+		_lastusedXpos = _screensize.width - 5;
 		_lastusedYpos = 5;
-	    }
+		_amount = 0;
+
+		_customaction = new AbstractAction() {
+			private static final long serialVersionUID = -7237306342417462544L;
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ChatFrame chatFrame = SparkManager.getChatManager().getChatContainer().getChatFrame();
+				chatFrame.setState(Frame.NORMAL);
+				chatFrame.setVisible(true);
+			}
+		};
 	}
 
-    }
+	@Override
+	public void messageReceived(ChatRoom room, Message message) {
+		RoarProperties props = RoarProperties.getInstance();
 
-    @Override
-    public void messageSent(ChatRoom room, Message message) {
-	// dont care
-    }
+		if (props.getShowingPopups() && (_amount < props.getMaximumPopups() || props.getMaximumPopups() == 0)) {
 
-    @Override
-    public void closingRoarPanel(int x, int y) {
-	if (_lastusedYpos > (y - 5)) {
-	    _lastusedYpos = y - 5;
+			ImageIcon icon = SparkRes.getImageIcon(SparkRes.SPARK_IMAGE_32x32);
+
+			String nickname = SparkManager.getUserManager().getUserNicknameFromJID(message.getFrom());
+			if (room.getChatType() == Message.Type.groupchat) {
+				nickname = StringUtils.parseResource(nickname);
+			}
+
+			boolean broadcast = message.getProperty("broadcast") != null;
+
+			if ((broadcast || message.getType() == Message.Type.normal || message.getType() == Message.Type.headline)
+					&& message.getBody() != null) {
+				nickname = Res.getString("broadcast") + " - " + nickname;
+			}
+
+			RoarPanel.popupWindow(this, icon, nickname, message.getBody(), _lastusedXpos, _lastusedYpos,
+					props.getDuration(), props.getBackgroundColor(), props.getHeaderColor(), props.getTextColor(),
+					_customaction);
+
+			++_amount;
+			_lastusedYpos += HEIGHT + 5;
+
+			if (_lastusedYpos >= _screensize.height - 90) {
+				_lastusedXpos -= WIDTH + 5;
+				_lastusedYpos = 5;
+			}
+		}
+
 	}
 
-	if (_lastusedXpos < (x + 5)) {
-	    _lastusedXpos = x + WIDTH + 5;
+	@Override
+	public void messageSent(ChatRoom room, Message message) {
+		// dont care
 	}
 
-	--_amount;
-	
-	if (_amount == 0) {
-	    _lastusedXpos = _screensize.width - 5;
-	    _lastusedYpos = 5;
+	@Override
+	public void closingRoarPanel(int x, int y) {
+		if (_lastusedYpos > (y - 5)) {
+			_lastusedYpos = y - 5;
+		}
+
+		if (_lastusedXpos < (x + 5)) {
+			_lastusedXpos = x + WIDTH + 5;
+		}
+
+		--_amount;
+
+		if (_amount == 0) {
+			_lastusedXpos = _screensize.width - 5;
+			_lastusedYpos = 5;
+		}
+
 	}
 
-    }
+	@Override
+	public String toString() {
+		return "TopRight";
+	}
 
-    @Override
-    public String toString() {
-	return "TopRight";
-    }
+	public static String getName() {
+		return "TopRight";
+	}
 
-    public static String getName() {
-	return "TopRight";
-    }
-
-    public static String getLocalizedName() {
-	return RoarResources.getString("roar.display.topright");
-    }
+	public static String getLocalizedName() {
+		return RoarResources.getString("roar.display.topright");
+	}
 
 }
