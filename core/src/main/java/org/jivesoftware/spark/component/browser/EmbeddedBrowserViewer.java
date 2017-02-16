898f7d4ba65a7406df41c20a748c7e31023c6924
Guus der Kinderen <guus.der.kinderen@gmail.com>
2017-02-16 21:57:10 +0100

    SPARK-1791: Replaced lobobrowser with JxBrowser (eval)
    The project for the embedded browser that we used, lobobrowser, is defunct. Maven artifacts are not to be found.
    
    A possible replacement is JxBrowser (by TeamDev). This software is proprietary, but they do offer free licenses to open source software.
    
    In order to evaluate wether we'd like to apply for one, this commit replaces lobobrowser with JxBrowser. An evaluation license is included, but will no longer be valid in 30 days from now.

--- core/src/main/java/org/jivesoftware/spark/component/browser/EmbeddedBrowserViewer.java
+++ core/src/main/java/org/jivesoftware/spark/component/browser/EmbeddedBrowserViewer.java
@@ -19,47 +19,29 @@ import java.awt.BorderLayout;
 import java.net.MalformedURLException;
 
 import javax.swing.JFrame;
-import javax.swing.LookAndFeel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import org.lobobrowser.gui.ContentEvent;
-import org.lobobrowser.gui.ContentListener;
-import org.lobobrowser.gui.FramePanel;
-import org.lobobrowser.main.PlatformInit;
+import com.teamdev.jxbrowser.chromium.*;
+import com.teamdev.jxbrowser.chromium.swing.BrowserView;
 
-public class EmbeddedBrowserViewer extends BrowserViewer implements ContentListener {
+public class EmbeddedBrowserViewer extends BrowserViewer {
 
 	private static final long serialVersionUID = -8055149462713514766L;
-	private FramePanel browser;
+	private Browser browser;
 	
 	/**
 	 * Constructs a new LobobrowserViewer
 	 */
 	public EmbeddedBrowserViewer() {
-		LookAndFeel laf = UIManager.getLookAndFeel();
-		try {
-			PlatformInit.getInstance().init(false, false);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	
-		browser = new FramePanel();
-		//substance look and feel 
-		try {
-				UIManager.setLookAndFeel(laf);
-			}
-			catch (UnsupportedLookAndFeelException e) {
-				e.printStackTrace();
-			}
-		browser.addContentListener(this);
+		browser = new Browser();
 	 }
 	
 	/**
 	 * Implementation of "Back"-button
 	 */
 	public void goBack() {
-		browser.back();
+		browser.goBack();
 	}
 
 	/**
@@ -67,30 +49,28 @@ public class EmbeddedBrowserViewer extends BrowserViewer implements ContentListe
 	 */
 	public void initializeBrowser() {
 		this.setLayout(new BorderLayout());
-		this.add(browser, BorderLayout.CENTER);
+
+		final BrowserView view = new BrowserView( browser );
+		this.add(view, BorderLayout.CENTER);
 	}
 
 	/**
 	 * Load the given URL
 	 */
 	public void loadURL(String url) {
-		try {
-			browser.navigate(url);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
+		browser.loadURL( url );
 	}
 
-	/**
-	 * React to an event by updating the address bar 
-	 */
-	public void contentSet(ContentEvent event) {
-		if (browser == null || browser.getCurrentNavigationEntry() == null) {
-            return;
-        }
-        String url = browser.getCurrentNavigationEntry().getUrl().toExternalForm();
-        documentLoaded(url);
-	}
+//	/**
+//	 * React to an event by updating the address bar
+//	 */
+//	public void contentSet(ContentEvent event) {
+//		if (browser == null || browser.getCurrentNavigationEntry() == null) {
+//            return;
+//        }
+//        String url = browser.getCurrentNavigationEntry().getUrl().toExternalForm();
+//        documentLoaded(url);
+//	}
 	
 	public static void main(String[] args) {
 		EmbeddedBrowserViewer  viewer = new EmbeddedBrowserViewer();
