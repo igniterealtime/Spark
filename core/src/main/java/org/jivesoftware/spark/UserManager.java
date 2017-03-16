99139932f08267182dcfe8c441f1847f701aa5cf
Alameyo <pawelsc@gmail.com>
2017-03-16 17:18:22 +0100

    Dialog moved to top-right (in way that it doesn't cover top menu bar).

--- core/src/main/java/org/jivesoftware/spark/UserManager.java
+++ core/src/main/java/org/jivesoftware/spark/UserManager.java
@@ -448,7 +448,7 @@ public class UserManager {
 
         final JPanel mainPanel = new JPanel(new BorderLayout());
         mainPanel.setLayout(new GridBagLayout());
-        mainPanel.add(layoutPanel, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 200, 0));
+        mainPanel.add(layoutPanel, new GridBagConstraints(0, 0, 1, 1, 0.5, 0.5, GridBagConstraints.NORTHEAST, GridBagConstraints.NONE, new Insets(20, 1, 5, 1), 200, 0));
         mainPanel.setOpaque(false);
 
         contactField.setText(contact);
