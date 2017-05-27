98967cb70497a3072473e9334b0fe93223d7bc2e
Alameyo <pawelsc@gmail.com>
2017-05-27 14:03:39 +0200

    SPARK-1926 and SPARK-1927 done, added new tab in login settings (Advenced).

---
+++ core/src/main/java/org/jivesoftware/spark/ui/login/CertificatesManagerSettingsPanel.java
@@ -0,0 +1,23 @@
+package org.jivesoftware.spark.ui.login;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JCheckBox;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+import org.jivesoftware.sparkimpl.settings.local.LocalPreferences;
+
+public class CertificatesManagerSettingsPanel extends JPanel implements ActionListener {
+	
+	public CertificatesManagerSettingsPanel(LocalPreferences localPreferences, JDialog optionsDialog) {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+	}
+
+}
