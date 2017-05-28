b387031c04fcc12f5154ac3e105e27132a807218
Alameyo <pawelsc@gmail.com>
2017-05-28 10:51:29 +0200

    Solves SPARK-1928, SPARK-1938 and part of SPARK-1937

--- core/src/main/java/org/jivesoftware/spark/ui/login/CertificatesManagerSettingsPanel.java
+++ core/src/main/java/org/jivesoftware/spark/ui/login/CertificatesManagerSettingsPanel.java
@@ -1,18 +1,42 @@
 package org.jivesoftware.spark.ui.login;
 
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
+import org.jivesoftware.resource.Res;
+import org.jivesoftware.sparkimpl.certificates.CertificateController;
+import org.jivesoftware.sparkimpl.certificates.CertificateModel;
 import org.jivesoftware.sparkimpl.settings.local.LocalPreferences;
 
 public class CertificatesManagerSettingsPanel extends JPanel implements ActionListener {
-	
+
+	private final static Insets DEFAULT_INSETS = new Insets(5, 5, 5, 5);
+	private final LocalPreferences localPreferences;
+	private JDialog optionsDialog;
+	private CertificateController certControll = new CertificateController();
+	private JTable certTable = new JTable();
+	private JScrollPane scrollPane;
+
 	public CertificatesManagerSettingsPanel(LocalPreferences localPreferences, JDialog optionsDialog) {
-		
+
+		this.localPreferences = localPreferences;
+		this.optionsDialog = optionsDialog;
+
+		certTable.setModel(certControll.getTableModel());
+		scrollPane = new JScrollPane(certTable);
+		certTable.setFillsViewportHeight(true);
+		this.add(scrollPane);
+
 	}
 
 	@Override
