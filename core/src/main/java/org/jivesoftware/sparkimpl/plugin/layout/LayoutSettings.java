fa316e4c74fa48b108d26a8dcf8ca1fb00833599
Guus der Kinderen <guus.der.kinderen@gmail.com>
2017-06-22 23:14:47 +0200

    SPARK-945: Save component bounds upon change
    This commit changes how component bounds (position + dimension) are stored. Before this commit, this
    occurred only when every component was closing. When closing Spark abruptly, these event listeners did not always trigger.
    
    With this commit, the bounds of components are updated whenever they change. The bounds are saved just once, when the main screen quits.

--- core/src/main/java/org/jivesoftware/sparkimpl/plugin/layout/LayoutSettings.java
+++ core/src/main/java/org/jivesoftware/sparkimpl/plugin/layout/LayoutSettings.java
@@ -1,11 +1,11 @@
-/**
+/*
  * Copyright (C) 2004-2011 Jive Software. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,143 +15,54 @@
  */
 package org.jivesoftware.sparkimpl.plugin.layout;
 
-import java.awt.Toolkit;
+import java.awt.*;
 
 
-public class LayoutSettings {
+public class LayoutSettings
+{
+    private Rectangle mainWindowBounds;
+    private Rectangle chatFrameBounds;
+    private Rectangle preferencesBounds;
 
-    private int mainWindowX;
-    private int mainWindowY;
-    private int mainWindowWidth;
-    private int mainWindowHeight;
+    private int splitPaneDividerLocation;
 
-    private int chatFrameX;
-    private int chatFrameY;
-    private int chatFrameWidth;
-    private int chatFrameHeight;
-
-    private int preferencesFrameX;
-    private int preferencesFrameY;
-    private int preferencesFrameWidth;
-    private int preferencesFrameHeight;
-    private int dividerLocation = -1;
-
-    public int getMainWindowX() {
-        return mainWindowX;
+    public Rectangle getMainWindowBounds()
+    {
+        return mainWindowBounds;
     }
 
-    public void setMainWindowX(int mainWindowX) {
-        this.mainWindowX = mainWindowX;
+    public void setMainWindowBounds( Rectangle mainWindowBounds )
+    {
+        this.mainWindowBounds = mainWindowBounds;
     }
 
-    public int getMainWindowY() {
-        return mainWindowY;
+    public Rectangle getChatFrameBounds()
+    {
+        return chatFrameBounds;
     }
 
-    public void setMainWindowY(int mainWindowY) {
-        this.mainWindowY = mainWindowY;
+    public void setChatFrameBounds( Rectangle chatFrameBounds )
+    {
+        this.chatFrameBounds = chatFrameBounds;
     }
 
-    public int getMainWindowWidth() {
-	if (mainWindowWidth < 100) {
-	    mainWindowWidth = 100;
-	} else if (mainWindowWidth > Toolkit.getDefaultToolkit()
-		.getScreenSize().width) {
-	    mainWindowWidth = Toolkit.getDefaultToolkit().getScreenSize().width - 50;
-	}
-	return mainWindowWidth;
+    public Rectangle getPreferencesBounds()
+    {
+        return preferencesBounds;
     }
 
-    public void setMainWindowWidth(int mainWindowWidth) {
-        this.mainWindowWidth = mainWindowWidth;
+    public void setPreferencesBounds( Rectangle preferencesBounds )
+    {
+        this.preferencesBounds = preferencesBounds;
     }
 
-    public int getMainWindowHeight() {
-	if (mainWindowHeight < 200) {
-	    mainWindowHeight = 500;
-	} else if (mainWindowHeight > Toolkit.getDefaultToolkit()
-		.getScreenSize().height) {
-	    mainWindowHeight = Toolkit.getDefaultToolkit().getScreenSize().height - 50;
-	}
-	return mainWindowHeight;
+    public int getSplitPaneDividerLocation()
+    {
+        return splitPaneDividerLocation;
     }
 
-    public void setMainWindowHeight(int mainWindowHeight) {
-        this.mainWindowHeight = mainWindowHeight;
+    public void setSplitPaneDividerLocation( int splitPaneDividerLocation )
+    {
+        this.splitPaneDividerLocation = splitPaneDividerLocation;
     }
-
-    public int getChatFrameX() {
-        return chatFrameX;
-    }
-
-    public void setChatFrameX(int chatFrameX) {
-        this.chatFrameX = chatFrameX;
-    }
-
-    public int getChatFrameY() {
-        return chatFrameY;
-    }
-
-    public void setChatFrameY(int chatFrameY) {
-        this.chatFrameY = chatFrameY;
-    }
-
-    public int getChatFrameWidth() {
-        return chatFrameWidth < 300 ? 300 : chatFrameWidth;
-    }
-
-    public void setChatFrameWidth(int chatFrameWidth) {
-        this.chatFrameWidth = chatFrameWidth;
-    }
-
-    public int getChatFrameHeight() {
-        return chatFrameHeight < 300 ? 300 : chatFrameHeight;
-    }
-
-    public void setChatFrameHeight(int chatFrameHeight) {
-        this.chatFrameHeight = chatFrameHeight;
-    }
-
-    public void setSplitPaneDividerLocation(int dividerLocation) {
-        this.dividerLocation = dividerLocation;
-
-    }
-
-    public int getPreferencesFrameX() {
-	return preferencesFrameX;
-    }
-
-    public void setPreferencesFrameX(int preferencesFrameX) {
-	this.preferencesFrameX = preferencesFrameX;
-    }
-
-    public int getPreferencesFrameY() {
-	return preferencesFrameY;
-    }
-
-    public void setPreferencesFrameY(int preferencesFrameY) {
-	this.preferencesFrameY = preferencesFrameY;
-    }
-
-    public int getPreferencesFrameWidth() {
-	return preferencesFrameWidth < 600 ? 600 : preferencesFrameWidth;
-    }
-
-    public void setPreferencesFrameWidth(int preferencesFrameWidth) {
-	this.preferencesFrameWidth = preferencesFrameWidth;
-    }
-
-    public int getPreferencesFrameHeight() {
-	return preferencesFrameHeight < 600 ? 600 : preferencesFrameHeight;
-    }
-
-    public void setPreferencesFrameHeight(int preferencesFrameHeight) {
-	this.preferencesFrameHeight = preferencesFrameHeight;
-    }
-
-    public int getSplitPaneDividerLocation() {
-	return dividerLocation;
-   }
-
-
 }
