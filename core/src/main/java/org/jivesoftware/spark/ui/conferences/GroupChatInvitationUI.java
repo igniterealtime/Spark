9193c7dd47d7bcc2818423c097749d6e4a04f418
arthur <ostretsov_av@hotmail.com>
2020-11-04 22:17:50 +0300

    SPARK-1469: Code cleanup: remove unused imports.

--- core/src/main/java/org/jivesoftware/spark/ui/conferences/GroupChatInvitationUI.java
+++ core/src/main/java/org/jivesoftware/spark/ui/conferences/GroupChatInvitationUI.java
@@ -22,7 +22,6 @@ import org.jivesoftware.smackx.muc.MultiUserChatManager;
 import org.jivesoftware.spark.ChatNotFoundException;
 import org.jivesoftware.spark.SparkManager;
 import org.jivesoftware.spark.component.RolloverButton;
-import org.jivesoftware.spark.ui.ChatRoom;
 import org.jivesoftware.spark.ui.rooms.GroupChatRoom;
 import org.jivesoftware.spark.util.ModelUtil;
 import org.jivesoftware.spark.util.SwingTimerTask;
