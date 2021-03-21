22bec1777531e3d3a89fdd9fc68838623a2f70b1
Amos Chepchieng <akchieng@gmail.com>
2021-03-21 12:10:58 +0300

    Attempted fix for offline contact taking over focus

--- core/src/main/java/org/jivesoftware/spark/ui/ChatContainer.java
+++ core/src/main/java/org/jivesoftware/spark/ui/ChatContainer.java
@@ -431,7 +431,8 @@ public class ChatContainer extends SparkTabbedPane implements MessageListener, C
 
         if (chatFrame.isVisible() && (chatFrame.getState() == Frame.ICONIFIED || chatFrame.getInactiveTime() > 20000)) {
             int tabLocation = indexOfComponent(chatRoom);
-            setSelectedIndex(tabLocation);
+            //setSelectedIndex(tabLocation);
+            System.err.println("Inactive for Long "+ tabLocation);
             groupChatMessageCheck(chatRoom, customMsg, customMsgText, customMsgTitle);
             return;
         }
