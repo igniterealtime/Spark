8cf20a0fe0aae63f920d84c829f546478ed2be0e
ilyaHlevnoy <71222850+Plyha@users.noreply.github.com>
2022-08-12 20:03:06 +0300

    SPARK-2277 Add control over IBB file transfer via ClientControl (#698)
    Since starting from Spark 3.0.0 IBB is disabled by default due to this bug https://igniterealtime.atlassian.net/browse/SPARK-2209  . But after updating the Smack library to version 4.4.X, I cannot reproduce this bug with the IBB setting. Perhaps some administrators may want to manage IBB settings via ClientControl plugin Openfire for their clients.

--- core/src/main/java/org/jivesoftware/gui/LoginUIPanel.java
+++ core/src/main/java/org/jivesoftware/gui/LoginUIPanel.java
@@ -1677,15 +1677,19 @@ public class LoginUIPanel extends javax.swing.JPanel implements KeyListener, Act
 
     private void persistEnterprise() {
         new Enterprise();
-        localPref.setAccountsReg(Enterprise.containsFeature(Enterprise.ACCOUNTS_REG_FEATURE));
-        localPref.setAdvancedConfig(Enterprise.containsFeature(Enterprise.ADVANCED_CONFIG_FEATURE));
-        localPref.setHostNameChange(Enterprise.containsFeature(Enterprise.HOST_NAME_FEATURE));
-        localPref.setInvisibleLogin(Enterprise.containsFeature(Enterprise.INVISIBLE_LOGIN_FEATURE));
-        localPref.setAnonymousLogin(Enterprise.containsFeature(Enterprise.ANONYMOUS_LOGIN_FEATURE));
-        localPref.setPswdAutologin(Enterprise.containsFeature(Enterprise.SAVE_PASSWORD_FEATURE));
-        if (Enterprise.containsFeature(Enterprise.HOSTNAME_AS_RESOURCE_FEATURE) != Enterprise.containsFeature(Enterprise.VERSION_AS_RESOURCE_FEATURE)) {
-            localPref.setUseHostnameAsResource(Enterprise.containsFeature(Enterprise.HOSTNAME_AS_RESOURCE_FEATURE));
-            localPref.setUseVersionAsResource(Enterprise.containsFeature(Enterprise.VERSION_AS_RESOURCE_FEATURE));
+        //Check that the ClientControl plugin Openfire is installed on the server.
+        if(Enterprise.isSparkManagerInstalled()){
+            localPref.setAccountsReg(Enterprise.containsFeature(Enterprise.ACCOUNTS_REG_FEATURE));
+            localPref.setAdvancedConfig(Enterprise.containsFeature(Enterprise.ADVANCED_CONFIG_FEATURE));
+            localPref.setHostNameChange(Enterprise.containsFeature(Enterprise.HOST_NAME_FEATURE));
+            localPref.setInvisibleLogin(Enterprise.containsFeature(Enterprise.INVISIBLE_LOGIN_FEATURE));
+            localPref.setAnonymousLogin(Enterprise.containsFeature(Enterprise.ANONYMOUS_LOGIN_FEATURE));
+            localPref.setPswdAutologin(Enterprise.containsFeature(Enterprise.SAVE_PASSWORD_FEATURE));
+            if (Enterprise.containsFeature(Enterprise.HOSTNAME_AS_RESOURCE_FEATURE) != Enterprise.containsFeature(Enterprise.VERSION_AS_RESOURCE_FEATURE)) {
+                localPref.setUseHostnameAsResource(Enterprise.containsFeature(Enterprise.HOSTNAME_AS_RESOURCE_FEATURE));
+                localPref.setUseVersionAsResource(Enterprise.containsFeature(Enterprise.VERSION_AS_RESOURCE_FEATURE));
+            }
+            localPref.setFileTransferIbbOnly(Enterprise.containsFeature(Enterprise.IBB_FEATURE));
         }
     }
 
