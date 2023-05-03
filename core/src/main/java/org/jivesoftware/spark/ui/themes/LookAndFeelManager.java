7ab78a05a7022f170e05b70eef9653685b81451d
ilya khlevnoy <71222850+Plyha@users.noreply.github.com>
2023-05-03 23:48:07 +0300

    add MacLight LaF

--- core/src/main/java/org/jivesoftware/spark/ui/themes/LookAndFeelManager.java
+++ core/src/main/java/org/jivesoftware/spark/ui/themes/LookAndFeelManager.java
@@ -10,7 +10,6 @@ import org.jivesoftware.sparkimpl.settings.local.SettingsManager;
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
-import org.jivesoftware.spark.ui.themes.lafs.SparkLightLaf;
 
 /**
  * Manages the Look and Feel instances that can be used by Spark.
@@ -25,7 +24,8 @@ public class LookAndFeelManager {
     public static final Class<? extends LookAndFeel> lafs[] = new Class[]{
         //flatlaf
         org.jivesoftware.spark.ui.themes.lafs.SparkLightLaf.class,
-        org.jivesoftware.spark.ui.themes.lafs.SparkIntelliJLaf.class
+        org.jivesoftware.spark.ui.themes.lafs.SparkIntelliJLaf.class,
+        org.jivesoftware.spark.ui.themes.lafs.SparkMacLightLaf.class
 };
 
     static {
