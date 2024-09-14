019ca9d685b5d85418fafe9bb51ea46cadb0755b
Sergey Ponomarev <stokito@gmail.com>
2024-09-14 22:41:00 +0300

    LoginUIPanel: copy header from LoginDialog

--- core/src/main/java/org/jivesoftware/gui/LoginUIPanel.java
+++ core/src/main/java/org/jivesoftware/gui/LoginUIPanel.java
@@ -1,7 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright (C) 2004-2011 Jive Software. All rights reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.jivesoftware.gui;
 
@@ -130,7 +140,8 @@ import org.minidns.dnsname.DnsName;
 import org.minidns.record.A;
 
 /**
- *
+ * Dialog to log in a user into the XMPP server.
+ * The LoginDialog is used only for login in registered users into the XMPP server.
  * @author KeepToo
  */
 public class LoginUIPanel extends javax.swing.JPanel implements KeyListener, ActionListener, FocusListener, CallbackHandler {
