9be82c2219b5ec98491ef5b512cd8d0deb4dc38a
Sergey Ponomarev <stokito@gmail.com>
2025-10-19 12:54:38 +0300

    ChatContainer.groupChatMessageCheck(): lazy init lastChatMessage;

--- core/src/main/java/org/jivesoftware/spark/ui/ChatContainer.java
+++ core/src/main/java/org/jivesoftware/spark/ui/ChatContainer.java
@@ -1127,7 +1127,7 @@ public class ChatContainer extends SparkTabbedPane implements MessageListener, C
         if (isGroupChat) {
             // is a group chat, perform some functions
             String fromNickName = "";
-            Message lastChatMessage = new Message();
+            Message lastChatMessage;
             Jid mucNickNameT;
             String finalRoomName = "";
             if (size > 0) {
@@ -1140,6 +1140,8 @@ public class ChatContainer extends SparkTabbedPane implements MessageListener, C
                 } else {
                     fromNickName = mucNickName[1];
                 }
+            } else {
+                lastChatMessage = new Message();
             }
             String myNickName = chatRoom.getNickname().toString();
             if (!myNickName.equals(fromNickName)) {
