24abfc9e9a20f0f54156615aa7d81c352272b115
Sergey Ponomarev <stokito@gmail.com>
2026-01-31 11:59:45 +0200

    startup.bat: remove jdic.jar

--- core/src/main/resources/startup.bat
+++ core/src/main/resources/startup.bat
@@ -8,14 +8,14 @@ if %PROCESSOR_ARCHITECTURE%==x86 (
 :run
 if "%1" == "-debug" goto debug
 if "%1" == "-noconsole" goto noconsole
-java -Dappdir=.. -cp ../lib/*;../lib/windows%SPARK64%/jdic.jar;../resources;../lib/windows%SPARK64%; -Djava.library.path="../lib/windows%SPARK64%/" org.jivesoftware.launcher.Startup
+java -Dappdir=.. -cp ../lib/*;../resources;../lib/windows%SPARK64%; -Djava.library.path="../lib/windows%SPARK64%/" org.jivesoftware.launcher.Startup
 goto end
 
 :noconsole
-javaw -Dappdir=.. -cp ../lib/*;../lib/windows%SPARK64%/jdic.jar;../resources;../lib/windows%SPARK64%; -Djava.library.path="../lib/windows%SPARK64%/" org.jivesoftware.launcher.Startup
+javaw -Dappdir=.. -cp ../lib/*;../resources;../lib/windows%SPARK64%; -Djava.library.path="../lib/windows%SPARK64%/" org.jivesoftware.launcher.Startup
 goto end
 
 :debug
-start "Spark" "%JAVA_HOME%\bin\java" -Ddebugger=true -Ddebug.mode=true -XX:+HeapDumpOnOutOfMemoryError -Xint -server -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -Dappdir=.. -cp ../lib/*;../lib/windows%SPARK64%/jdic.jar;../resources;../lib/windows%SPARK64%/; org.jivesoftware.launcher.Startup
+start "Spark" "%JAVA_HOME%\bin\java" -Ddebugger=true -Ddebug.mode=true -XX:+HeapDumpOnOutOfMemoryError -Xint -server -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -Dappdir=.. -cp ../lib/*;../resources;../lib/windows%SPARK64%/; org.jivesoftware.launcher.Startup
 goto end
 :end
