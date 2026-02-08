2a80e35636e285f983a64997dc67bdf52f7c738f
Sergey Ponomarev <stokito@gmail.com>
2026-02-08 08:20:34 +0200

    AnswerFormDialog: Use DataFormUI

--- core/src/main/java/org/jivesoftware/spark/ui/conferences/AnswerFormDialog.java
+++ core/src/main/java/org/jivesoftware/spark/ui/conferences/AnswerFormDialog.java
@@ -15,128 +15,78 @@
  */
 package org.jivesoftware.spark.ui.conferences;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.util.HashMap;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import java.awt.*;
+import javax.swing.*;
 
 import org.jivesoftware.resource.Res;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smackx.xdata.FormField;
 import org.jivesoftware.smackx.xdata.form.FillableForm;
 import org.jivesoftware.smackx.xdata.packet.DataForm;
 import org.jivesoftware.smackx.muc.MultiUserChat;
-import org.jivesoftware.spark.ChatManager;
 import org.jivesoftware.spark.SparkManager;
 import org.jivesoftware.spark.ui.ChatRoom;
+import org.jivesoftware.spark.ui.DataFormUI;
 import org.jivesoftware.spark.util.GraphicUtils;
 import org.jivesoftware.spark.util.ResourceUtils;
 
+import static org.jivesoftware.spark.ChatManager.ERROR_COLOR;
+import static org.jivesoftware.spark.ChatManager.NOTIFICATION_COLOR;
+
 /**
- * Answer Form Dialog
+ * Answer Form Dialog.
+ * Used for registration in the room.
  *
  * @author wolf.posdorfer
  */
 public class AnswerFormDialog {
+    private final DataFormUI dataFormUI;
     private final JDialog dialog;
-    private final JPanel centerPanel;
-
-    private final HashMap<String, JComponent> _map = new HashMap<>();
 
     public AnswerFormDialog(JFrame parent, final MultiUserChat chat, final DataForm form) {
-        centerPanel = new JPanel();
-        JPanel bottomPanel = new JPanel();
-        centerPanel.setLayout(new GridBagLayout());
-
+        this.dataFormUI = new DataFormUI(form);
         dialog = new JDialog(parent, true);
         dialog.setTitle(Res.getString("button.register").replace("&", ""));
-
-        int row = 0;
-        for (final FormField formfield : form.getFields()) {
-            JLabel label = new JLabel(formfield.getLabel());
-            FormField.Type type = formfield.getType();
-
-            JComponent comp = null;
-            if (type == FormField.Type.text_single) {
-                comp = new JTextField();
-            } else if (type == FormField.Type.text_multi) {
-                comp = new JTextArea();
-                comp.setBorder(new JTextField().getBorder());
-            }
-
-            if (comp != null) {
-                addComponent(label, comp, row, formfield.getFieldName());
-                row++;
-            }
-        }
+        dialog.setLayout(new GridBagLayout());
 
         JButton updateButton = new JButton();
         ResourceUtils.resButton(updateButton, Res.getString("apply"));
         updateButton.addActionListener(e -> {
             dialog.dispose();
-            sendAnswerForm(new FillableForm(form), chat);
+            sendAnswerForm(chat);
         });
 
+        JButton cancelButton = new JButton();
+        ResourceUtils.resButton(cancelButton, Res.getString("button.cancel"));
+        cancelButton.addActionListener(actionEvent -> dialog.dispose());
+
+        JPanel bottomPanel = new JPanel();
         bottomPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
         bottomPanel.add(updateButton);
+        bottomPanel.add(cancelButton);
 
         dialog.getContentPane().setLayout(new BorderLayout());
-        dialog.getContentPane().add(centerPanel, BorderLayout.CENTER);
+        dialog.getContentPane().add(dataFormUI, BorderLayout.CENTER);
         dialog.getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+
         dialog.pack();
-        dialog.setSize(600, 400);
+//        dialog.setSize(600, 400);
         GraphicUtils.centerWindowOnScreen(dialog);
         dialog.setVisible(true);
     }
 
-    private void addComponent(JLabel label, JComponent comp, int row, String variable) {
-        centerPanel.add(label, new GridBagConstraints(0, row, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 50, 0));
-        centerPanel.add(comp, new GridBagConstraints(1, row, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 50, 0));
-        _map.put(variable, comp);
-    }
-
     /**
      * Sends the Answer Form
-     * @param answer must be an answer-form
      */
-    private void sendAnswerForm(FillableForm answer, MultiUserChat chat) {
+    private void sendAnswerForm(MultiUserChat chat) {
         ChatRoom room = SparkManager.getChatManager().getChatRoom(chat.getRoom());
-        for (String key : _map.keySet()) {
-            String value = getValueFromComponent(key);
-            answer.setAnswer(key, value);
-        }
+        FillableForm filledForm = dataFormUI.getFilledForm();
         try {
-            chat.sendRegistrationForm(answer);
+            chat.sendRegistrationForm(filledForm);
             String reg = Res.getString("message.groupchat.registered.member", chat.getRoom());
-            room.getTranscriptWindow().insertNotificationMessage(reg, ChatManager.NOTIFICATION_COLOR);
+            room.getTranscriptWindow().insertNotificationMessage(reg, NOTIFICATION_COLOR);
         } catch (XMPPException | SmackException | InterruptedException e) {
-            room.getTranscriptWindow().insertNotificationMessage(e.getMessage(), ChatManager.ERROR_COLOR);
-        }
-    }
-
-    /**
-     * returns the Component specific value
-     */
-    private String getValueFromComponent(String label) {
-        Component comp = _map.get(label);
-        if (comp instanceof JTextField) {
-            return ((JTextField) comp).getText();
-        } else if (comp instanceof JTextArea) {
-            return ((JTextArea) comp).getText();
-        } else {
-            return null;
+            room.getTranscriptWindow().insertNotificationMessage(e.getMessage(), ERROR_COLOR);
         }
     }
 
