c09dae8079a0a63ef022edbffa7bd9c9f96f39e8
Sergey Ponomarev <stokito@gmail.com>
2026-02-11 23:28:05 +0200

    SPARK-1605: Show the Attention Buzz button only if the contact's client supports (enabled) it

--- core/src/main/java/org/jivesoftware/sparkimpl/plugin/alerts/BuzzPlugin.java
+++ core/src/main/java/org/jivesoftware/sparkimpl/plugin/alerts/BuzzPlugin.java
@@ -20,14 +20,21 @@ import org.jivesoftware.smack.filter.AndFilter;
 import org.jivesoftware.smack.filter.StanzaTypeFilter;
 import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smackx.attention.packet.AttentionExtension;
+import org.jivesoftware.smackx.disco.ServiceDiscoveryManager;
+import org.jivesoftware.smackx.disco.packet.DiscoverInfo;
+import org.jivesoftware.spark.Event;
+import org.jivesoftware.spark.PresenceManager;
+import org.jivesoftware.spark.SoundManager;
 import org.jivesoftware.spark.SparkManager;
 import org.jivesoftware.spark.plugin.Plugin;
 import org.jivesoftware.spark.ui.*;
 import org.jivesoftware.spark.ui.rooms.ChatRoomImpl;
 import org.jivesoftware.spark.util.SwingTimerTask;
 import org.jivesoftware.spark.util.TaskEngine;
+import org.jivesoftware.spark.util.log.Log;
 import org.jivesoftware.sparkimpl.settings.local.SettingsManager;
 import org.jxmpp.jid.EntityBareJid;
+import org.jxmpp.jid.EntityFullJid;
 
 import java.util.TimerTask;
 
@@ -36,8 +43,11 @@ import javax.swing.SwingUtilities;
 import static org.jivesoftware.spark.ChatManager.NOTIFICATION_COLOR;
 
 /**
- * Allows users to shake a users frame.
- * XEP-0224 Attention
+ * Allows users to shake a user's frame to attract their attention and play a buzz sound.
+ * It's kind of "call" to get an immediate answer.
+ * Such a notification may be shown even in DND status.
+ * A user may disable the buzz feature in the settings, but anyway it will see the buzz as a regular message.
+ * @see <a href="https://xmpp.org/extensions/xep-0224.html">XEP-0224 Attention</a>.
  */
 public class BuzzPlugin implements Plugin {
 
@@ -89,12 +99,29 @@ public class BuzzPlugin implements Plugin {
 
     private void addBuzzFeatureToChatRoom(final ChatRoom room) {
         if (room instanceof ChatRoomImpl) {
-            // Add Button to toolbar
-            if (!SettingsManager.getLocalPreferences().isBuzzEnabled()) {
+            boolean hasAttentionSupport = clientOfContactSupportsAttentions(room);
+            if (!hasAttentionSupport) {
                 return;
             }
+            // Add the button to the toolbar
             new BuzzRoomDecorator(room);
         }
+    }
+
+    /**
+     * Determine via service discovery if the contact's client supports attentions
+     */
+    private static boolean clientOfContactSupportsAttentions(ChatRoom room) {
+        EntityFullJid fullJID = PresenceManager.getFullyQualifiedJID(room.getBareJid());
+        boolean hasAttentionSupport = true;
+        ServiceDiscoveryManager discoManager = SparkManager.getDiscoManager();
+        try {
+            DiscoverInfo discoverInfo = discoManager.discoverInfo(fullJID);
+            hasAttentionSupport = discoverInfo.containsFeature(AttentionExtension.NAMESPACE);
+        } catch (Exception e) {
+            Log.warning(e.getMessage());
+        }
+        return hasAttentionSupport;
     }
 
     private void shakeWindow(Message message) {
@@ -122,6 +149,7 @@ public class BuzzPlugin implements Plugin {
             if (SettingsManager.getLocalPreferences().isBuzzEnabled()) {
                 chatFrame.buzz();
                 chatContainer.activateChatRoom(room);
+//TODO                SparkManager.getSoundManager().playClip(Event.ATTENTION_BUZZ);
             }
         }
 
