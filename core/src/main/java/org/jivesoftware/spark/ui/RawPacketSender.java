8cb1ad4153d8302e9d162fc4b872117230447e93
Sergey Ponomarev <stokito@gmail.com>
2026-02-13 17:25:04 +0200

    RawPacketSender: autogenerate stanza id, add stanza samples, autocapture response

--- core/src/main/java/org/jivesoftware/spark/ui/RawPacketSender.java
+++ core/src/main/java/org/jivesoftware/spark/ui/RawPacketSender.java
@@ -23,8 +23,11 @@ import java.awt.GridLayout;
 import javax.swing.*;
 
 import org.jivesoftware.resource.SparkRes;
+import org.jivesoftware.smack.filter.StanzaFilter;
 import org.jivesoftware.smack.packet.Stanza;
 import org.jivesoftware.smack.packet.XmlEnvironment;
+import org.jivesoftware.smack.packet.id.StandardStanzaIdSource;
+import org.jivesoftware.smack.util.XmlUtil;
 import org.jivesoftware.spark.SparkManager;
 import org.jivesoftware.spark.component.MessageDialog;
 
@@ -35,6 +38,25 @@ import org.jivesoftware.spark.component.MessageDialog;
  */
 public class RawPacketSender {
 
+    private static final String[] STANZA_SAMPLES = {
+        "<presence xmlns=\"jabber:client\" id=\"\">\n" +
+            "<show></show>\n" +
+            "<status></status>\n" +
+            "<priority></priority>\n" +
+            "</presence>",
+        "<message to=\"\" type=\"\" xmlns=\"jabber:client\" id=\"\">\n" +
+            "<body></body>\n" +
+            "</message>",
+        "<iq to=\"\" type=\"\" xmlns=\"jabber:client\" id=\"\">\n" +
+            "<query xmlns=\"\"></query>\n" +
+            "</iq>",
+        "<iq to=\"\" type=\"get\" xmlns=\"jabber:client\" id=\"\">\n" +
+            "<query xmlns=\"http://jabber.org/protocol/disco#info\"></query>\n" +
+            "</iq>",
+        "<iq to=\"\" type=\"get\" xmlns=\"jabber:client\" id=\"\">\n" +
+            "<query xmlns=\"jabber:iq:version\"></query>\n" +
+            "</iq>"
+    };
     private final JFrame _mainFrame;
     private final JPanel _mainPanel;
 
@@ -44,6 +66,7 @@ public class RawPacketSender {
     private final JTextArea _inputArea;
     private final JButton _btnSend;
     private final JButton _btnClearLog;
+    private final JComboBox<String> _cbStanzas;
 
     public RawPacketSender() {
         _mainFrame = new JFrame("Send Raw Packets");
@@ -52,16 +75,26 @@ public class RawPacketSender {
         _mainPanel.setLayout(new GridLayout(2, 1));
         _textAreaLog = new JTextArea();
         _inputArea = new JTextArea();
+        _inputArea.setToolTipText("Leave the stanza id attribute empty to generate a new id");
         _textScroller = new JScrollPane(_textAreaLog);
         _btnSend = new JButton("Send", SparkRes.getImageIcon(SparkRes.SMALL_CHECK));
         _btnSend.addActionListener(e -> onBtnSendClick());
         _btnClearLog = new JButton("Clear", SparkRes.getImageIcon(SparkRes.SMALL_DELETE));
         _btnClearLog.addActionListener(e -> onBtnClearLogClick());
+        _cbStanzas = new JComboBox<>(new String[] {
+            "Presence",
+            "Message",
+            "IQ",
+            "Discovery Info",
+            "Get version",
+        });
+        _cbStanzas.addActionListener(e -> _inputArea.setText(STANZA_SAMPLES[_cbStanzas.getSelectedIndex()]));
 
         JPanel southPanel = new JPanel(new BorderLayout());
         southPanel.add(_inputArea, BorderLayout.CENTER);
 
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        buttonPanel.add(_cbStanzas);
         buttonPanel.add(_btnClearLog);
         buttonPanel.add(_btnSend);
         southPanel.add(buttonPanel, BorderLayout.SOUTH);
@@ -74,6 +107,15 @@ public class RawPacketSender {
         _mainFrame.setSize(500, 500);
         _mainFrame.setLocationRelativeTo(null);
         _mainFrame.setVisible(true);
+        // handle request and responses with the debugging it "id-X"
+        StanzaFilter debugPacketFilter = stanza -> stanza.getStanzaId() != null && stanza.getStanzaId().endsWith("-X");
+        SparkManager.getConnection().addAsyncStanzaListener(this::addRespToLog, debugPacketFilter);
+    }
+
+    private void addRespToLog(Stanza stanza) {
+        SwingUtilities.invokeLater(() -> {
+            _textAreaLog.append("\n" + XmlUtil.prettyFormatXml(stanza.toXML()));
+        });
     }
 
     private void onBtnSendClick() {
@@ -81,11 +123,13 @@ public class RawPacketSender {
         if (reqText.isBlank()) {
             return;
         }
-        String rawXml = reqText;
+        String debugStanzaId = StandardStanzaIdSource.DEFAULT.getNewStanzaId() + "-X";
+        // replace empty id with generated id
+        String rawXml = reqText.replace("id=\"\"", "id=\"" + debugStanzaId + "\"");
         AdHocPacket packetToSend = new AdHocPacket(rawXml);
         try {
             SparkManager.getConnection().sendStanza(packetToSend);
-            _textAreaLog.append("\n" + rawXml);
+            _textAreaLog.append("\n" + XmlUtil.prettyFormatXml(rawXml));
         } catch (Exception exc) {
             MessageDialog.showErrorDialog(_mainFrame, exc);
         }
