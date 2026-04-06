a1ccf74d87977a547df534938fd778f593b7c8f6
Sergey Ponomarev <stokito@gmail.com>
2026-04-06 22:01:12 +0300

    SPARK-1993: Log: remove unused handlers from SPARK-1486 and print warnings to error log

--- core/src/main/java/org/jivesoftware/spark/util/log/Log.java
+++ core/src/main/java/org/jivesoftware/spark/util/log/Log.java
@@ -20,9 +20,7 @@ import org.jivesoftware.Spark;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.FileHandler;
-import java.util.logging.Handler;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 /**
@@ -31,7 +29,6 @@ import java.util.logging.SimpleFormatter;
  */
 public class Log {
 	private final java.util.logging.Logger ERROR_LOGGER;
-	private final Logger WARNING_LOGGER;
     private static final boolean debugEnabled = System.getProperty("debug.mode") != null;
 
 	private volatile static Log singleton = null;
@@ -52,51 +49,23 @@ public class Log {
 			Spark.getLogDirectory().mkdirs();
 		}
 		ERROR_LOGGER = java.util.logging.Logger.getAnonymousLogger();
-		WARNING_LOGGER = java.util.logging.Logger.getAnonymousLogger();
-
 		File ERROR_LOG_FILE = new File(Spark.getLogDirectory(), "errors.log");
-		File WARNING_LOG_FILE = new File(Spark.getLogDirectory(), "warn.log");
-
 		try {
 			// Create an appending file handler
 			boolean append = true;
 			FileHandler errorHandler = new FileHandler(
 					ERROR_LOG_FILE.getCanonicalPath(), 1000000, 10, append);
 			errorHandler.setFormatter(new SimpleFormatter());
-
-			FileHandler warnHandler = new FileHandler(
-					WARNING_LOG_FILE.getCanonicalPath(), 1000000, 10, append);
-			warnHandler.setFormatter(new SimpleFormatter());
-
 			// Add to the desired logger
 			ERROR_LOGGER.addHandler(errorHandler);
-			WARNING_LOGGER.addHandler(warnHandler);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	/**
-	 * Adds a handler to the error logger
-	 * 
-	 * @param h the handler to add
-	 */
-	public synchronized static void addErrorHandler(Handler h) {
-		getInstance().ERROR_LOGGER.addHandler(h);
-	}
-
-	/**
-	 * Adds a handler to the error logger
-	 * 
-	 * @param h the handler to add
-	 */
-	public synchronized static void addWarningHandler(Handler h) {
-		getInstance().WARNING_LOGGER.addHandler(h);
-	}
-
-	/**
 	 * Logs all error messages to default error logger.
-	 * 
+	 *
 	 * @param message
 	 *            a message to append to log file.
 	 * @param ex the exception being thrown.
@@ -107,7 +76,7 @@ public class Log {
 
 	/**
 	 * Logs all error messages to default error logger.
-	 * 
+	 *
 	 * @param ex the exception being thrown.
 	 */
 	public static void error(Throwable ex) {
@@ -116,21 +85,21 @@ public class Log {
 
 	/**
 	 * Log a warning message to the default logger.
-	 * 
+	 *
 	 * @param message the message to log.
 	 * @param ex the exception.
 	 */
 	public static void warning(String message, Throwable ex) {
-		getInstance().WARNING_LOGGER.log(Level.WARNING, message, ex);
+		getInstance().ERROR_LOGGER.log(Level.WARNING, message, ex);
 	}
 
 	public static void warning(String message) {
-		getInstance().WARNING_LOGGER.log(Level.WARNING, message);
+		getInstance().ERROR_LOGGER.log(Level.WARNING, message);
 	}
 
 	/**
 	 * Logs all error messages to default error logger.
-	 * 
+	 *
 	 * @param message a message to append to log file.
 	 */
 	public static void error(String message) {
@@ -142,7 +111,7 @@ public class Log {
 	 * in the VM Parameters debug.mode=true.
 	 * <p/>
 	 * ex. (-Ddebug.mode=true)
-	 * 
+	 *
 	 * @param message the message to print out.
 	 */
 	public static void debug(String message) {
