5ed8e74a72c7d3562d77d1a8061cba056bca6282
Sergey Ponomarev <stokito@gmail.com>
2026-04-19 14:50:39 +0300

    SoundPlugin: Use Roster PresenceEventListener instead of parsing of a raw presence packet

--- core/src/main/java/org/jivesoftware/sparkimpl/preference/sounds/SoundPlugin.java
+++ core/src/main/java/org/jivesoftware/sparkimpl/preference/sounds/SoundPlugin.java
@@ -16,12 +16,11 @@
 
 package org.jivesoftware.sparkimpl.preference.sounds;
 
-import org.jivesoftware.smack.filter.StanzaTypeFilter;
 import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smack.roster.PresenceEventListener;
 import org.jivesoftware.smackx.delay.packet.DelayInformation;
 import org.jivesoftware.smackx.muc.MultiUserChatManager;
-import org.jivesoftware.spark.PresenceManager;
 import org.jivesoftware.spark.SparkManager;
 import org.jivesoftware.spark.plugin.Plugin;
 import org.jivesoftware.spark.ui.ChatRoom;
@@ -30,6 +29,8 @@ import org.jivesoftware.spark.ui.MessageListener;
 import org.jivesoftware.spark.util.TaskEngine;
 import org.jxmpp.jid.BareJid;
 import org.jxmpp.jid.EntityBareJid;
+import org.jxmpp.jid.FullJid;
+import org.jxmpp.jid.Jid;
 
 import static org.jivesoftware.spark.Event.*;
 
@@ -42,7 +43,7 @@ import static org.jivesoftware.spark.Event.*;
 public class SoundPlugin implements Plugin {
 
     @Override
-	public void initialize() {
+    public void initialize() {
         SoundPreference soundPreference = new SoundPreference();
         SparkManager.getPreferenceManager().addPreference(soundPreference);
         // Load sound preferences.
@@ -80,20 +81,37 @@ public class SoundPlugin implements Plugin {
             }
         });
 
-        SparkManager.getConnection().addAsyncStanzaListener( stanza -> {
-            Presence presence = (Presence)stanza;
-            BareJid presenceBareJid = presence.getFrom().asBareJid();
-            // Ignore own presence updates
-            if (presenceBareJid.equals(myBareJid)) {
-                return;
+        SparkManager.getRoster().addPresenceEventListener(new PresenceEventListener() {
+            @Override
+            public void presenceAvailable(FullJid from, Presence presence) {
             }
 
-            if (!presence.isAvailable()) {
-                if (!PresenceManager.isOnline(presenceBareJid)) {
-                    SparkManager.getSoundManager().playClip(STATUS_OFFLINE);
+            @Override
+            public void presenceUnavailable(FullJid from, Presence presence) {
+                // When the shutdown is ongoing ignore all the events
+                if (!SparkManager.getConnection().isConnected()) {
+                    return;
                 }
+                BareJid presenceBareJid = from.asBareJid();
+                // Ignore own presence updates
+                if (presenceBareJid.equals(myBareJid)) {
+                    return;
+                }
+                SparkManager.getSoundManager().playClip(STATUS_OFFLINE);
             }
-        }, new StanzaTypeFilter(Presence.class));
+
+            @Override
+            public void presenceError(Jid address, Presence errorPresence) {
+            }
+
+            @Override
+            public void presenceSubscribed(BareJid address, Presence subscribedPresence) {
+            }
+
+            @Override
+            public void presenceUnsubscribed(BareJid address, Presence unsubscribedPresence) {
+            }
+        });
 
         MultiUserChatManager mucManager = SparkManager.getMucManager();
         mucManager.addInvitationListener( ( xmppConnection, muc, inviter, reason, password, message, invitation ) -> {
