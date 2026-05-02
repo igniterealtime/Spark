f762548781b9c036152891b945aa58c7b4b456d7
Sergey Ponomarev <stokito@gmail.com>
2026-05-02 19:16:52 +0300

    Add Signal transport and SVG icons

---
+++ core/src/main/java/org/jivesoftware/sparkimpl/plugin/gateways/transports/SignalTransport.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (C) 2026 Ignite Realtime. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jivesoftware.sparkimpl.plugin.gateways.transports;
+
+import org.jivesoftware.resource.Res;
+import org.jivesoftware.resource.SparkRes;
+import org.jxmpp.jid.DomainBareJid;
+
+import javax.swing.*;
+
+public class SignalTransport extends Transport {
+    public SignalTransport(DomainBareJid xmppServiceDomain, String xmppServiceName) {
+        super(xmppServiceDomain, xmppServiceName);
+    }
+
+    @Override
+	public String getTitle(){
+        return Res.getString("message.register.transports");
+    }
+
+    @Override
+	public String getInstructions() {
+        return Res.getString("message.enter.gateway");
+    }
+
+    @Override
+	public Icon getIcon() {
+        return SparkRes.getImageIcon(SparkRes.Icon.SIGNAL_TRANSPORT_ACTIVE_IMAGE);
+    }
+
+    @Override
+	public Icon getInactiveIcon() {
+        return SparkRes.getImageIcon(SparkRes.Icon.DISCORD_TRANSPORT_INACTIVE_IMAGE);
+    }
+
+    @Override
+	public String getName(){
+        return "Signal";
+    }
+
+    @Override
+	public boolean requiresUsername() {
+        return true;
+    }
+
+    @Override
+	public boolean requiresPassword() {
+        return true;
+    }
+
+    @Override
+	public boolean requiresNickname() {
+        return false;
+    }
+
+}
