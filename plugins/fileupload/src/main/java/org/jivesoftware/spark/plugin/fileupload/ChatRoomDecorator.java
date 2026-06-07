284e29a9f5ee4c4514d3bcd13559f3f8f2243f22
Sergey Ponomarev <stokito@gmail.com>
2026-06-07 15:19:23 +0300

    fileupload plugin: simplify

--- plugins/fileupload/src/main/java/org/jivesoftware/spark/plugin/fileupload/ChatRoomDecorator.java
+++ plugins/fileupload/src/main/java/org/jivesoftware/spark/plugin/fileupload/ChatRoomDecorator.java
@@ -18,7 +18,6 @@ package org.jivesoftware.spark.plugin.fileupload;
 
 import org.jivesoftware.resource.Res;
 import org.jivesoftware.resource.SparkRes;
-import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.MessageBuilder;
 import org.jivesoftware.smack.packet.StanzaBuilder;
 import org.jivesoftware.smack.packet.StandardExtensionElement;
@@ -28,15 +27,13 @@ import org.jivesoftware.spark.ui.ChatRoom;
 import org.jivesoftware.spark.util.GraphicUtils;
 import org.jivesoftware.spark.util.log.Log;
 
-import javax.swing.*;
-import java.awt.*;
-import java.io.*;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import java.awt.FileDialog;
+import java.awt.Frame;
+import java.io.File;
 import java.net.URL;
-import java.net.URLConnection;
-import java.nio.file.Files;
-
-import static org.jivesoftware.smack.packet.Message.Type.groupchat;
-import static org.jivesoftware.smack.packet.Message.Type.chat;
 
 public class ChatRoomDecorator {
     private final HttpFileUploadManager httpFileUploadManager;
@@ -51,7 +48,7 @@ public class ChatRoomDecorator {
         try {
             fileuploadButton.setToolTipText(GraphicUtils.createToolTip("Http File Upload"));
             fileuploadButton.addActionListener(event -> {
-                getUploadUrl(room, room.getChatType() == groupchat ? groupchat : chat);
+                getUploadUrl(room);
             });
             room.getEditorBar().add(fileuploadButton);
         } catch (Exception e) {
@@ -59,45 +56,26 @@ public class ChatRoomDecorator {
         }
     }
 
-    private static String guessContentType(final File file) {
-        String result = URLConnection.guessContentTypeFromName(file.getName());
-        if (result != null && !result.isEmpty()) {
-            return result;
-        }
-        try {
-            result = Files.probeContentType(file.toPath());
-            if (result != null && !result.isEmpty()) {
-                return result;
-            }
-            try (final InputStream is = new BufferedInputStream(Files.newInputStream(file.toPath()))) {
-                result = URLConnection.guessContentTypeFromStream(is);
-            }
-            return result;
-        } catch (IOException e) {
-            return null;
-        }
-    }
-
     public void finished() {
         Log.debug("ChatRoomDecorator finished for room: " + room.getBareJid());
     }
 
-    private void getUploadUrl(ChatRoom room, Message.Type type) {
+    private void getUploadUrl(ChatRoom room) {
         FileDialog fd = new FileDialog((Frame) null, "Choose a file to upload", FileDialog.LOAD);
         fd.setMultipleMode(true);
         fd.setVisible(true);
         File[] files = fd.getFiles();
 
         for (File file : files) {
-            SwingUtilities.invokeLater(() -> handleUpload(file, room, type));
+            SwingUtilities.invokeLater(() -> handleUpload(file, room));
         }
     }
 
-    private void handleUpload(File file, ChatRoom room, Message.Type type) {
+    private void handleUpload(File file, ChatRoom room) {
         Log.debug("Uploading file: " + file.getAbsolutePath());
         try {
             URL uploadedFile = httpFileUploadManager.uploadFile(file);
-            broadcastUploadUrl(uploadedFile.toString(), type);
+            broadcastUploadUrl(uploadedFile.toString());
         } catch (Exception e) {
             Log.error("Error while attempting to uploading file", e);
             UIManager.put("OptionPane.okButtonText", Res.getString("ok"));
@@ -108,9 +86,8 @@ public class ChatRoomDecorator {
         }
     }
 
-    private void broadcastUploadUrl(String url, Message.Type type) {
+    private void broadcastUploadUrl(String url) {
         MessageBuilder messageBuilder = StanzaBuilder.buildMessage()
-            .ofType(type)
             .setBody(url)
             .addExtension(
                 StandardExtensionElement.builder("x", "jabber:x:oob")
