bb970d5eaf6a835d53fed645bfc7db7a87f35833
Sergey Ponomarev <stokito@gmail.com>
2026-07-11 12:46:30 +0300

    cleanup BroadcastHistoryFrame

--- core/src/main/java/org/jivesoftware/spark/ui/BroadcastHistoryFrame.java
+++ core/src/main/java/org/jivesoftware/spark/ui/BroadcastHistoryFrame.java
@@ -1,37 +1,43 @@
 package org.jivesoftware.spark.ui;
+
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import javax.swing.GroupLayout;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JToggleButton;
+
 import org.jivesoftware.Spark;
 import org.jivesoftware.resource.Res;
 import org.jivesoftware.spark.util.log.Log;
 import org.jivesoftware.sparkimpl.plugin.layout.LayoutSettingsManager;
 
+import static java.awt.EventQueue.invokeLater;
+import static javax.swing.LayoutStyle.ComponentPlacement;
+
 /**
  * @author ps
  */
-public class BroadcastHistoryFrame extends javax.swing.JFrame {
+public class BroadcastHistoryFrame extends JFrame {
+    private JFormattedTextField dateField;
+    private JTextArea broadcastHistoryArea;
+    private JLabel searchDate;
+    private JToggleButton searchButton;
+
     private final File transcriptsFolder;
 
-    /**
-     * Creates new form NewJFrame
-     */
     public BroadcastHistoryFrame() {
         transcriptsFolder = new File(Spark.getUserProfileFolder(), "transcripts");
-        BroadcastHistoryArea = new javax.swing.JTextArea();
-        BroadcastHistoryArea.setEditable(false);
-        BroadcastHistoryArea.setLineWrap(true);
-        BroadcastHistoryArea.setWrapStyleWord(true);
         initComponents();
     }
 
@@ -40,126 +46,113 @@ public class BroadcastHistoryFrame extends javax.swing.JFrame {
         if (!myfile.exists()) {
             return;
         }
-        FileInputStream fis = new FileInputStream(myfile);
-        BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-
-        String line;
-        while ((line = br.readLine()) != null) {
-            BroadcastHistoryArea.append(line + "\n");
-        }
-        br.close();
+        String content = Files.readString(myfile.toPath());
+        broadcastHistoryArea.setText(content);
     }
-    
-    private void initComponents() {        
-        SearchButton = new javax.swing.JToggleButton();
-        DateField = new javax.swing.JFormattedTextField();
-        SearchDate = new javax.swing.JLabel();
-        Date date=new Date();
+
+    private void initComponents() {
+        broadcastHistoryArea = new JTextArea();
+        broadcastHistoryArea.setEditable(false);
+        broadcastHistoryArea.setLineWrap(true);
+        broadcastHistoryArea.setWrapStyleWord(true);
+
+        searchButton = new JToggleButton();
+        dateField = new JFormattedTextField();
+        searchDate = new JLabel();
+        Date date = new Date();
         Format formatter = new SimpleDateFormat("yyy-MM");
         String myDate = formatter.format(date);
-        DateField.setValue(myDate);
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        JScrollPane panelPane = new JScrollPane(BroadcastHistoryArea);
-        SearchDate.setText(Res.getString("label.broadcast.history.search.date"));
+        dateField.setValue(myDate);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        JScrollPane panelPane = new JScrollPane(broadcastHistoryArea);
+        searchDate.setText(Res.getString("label.broadcast.history.search.date"));
         setTitle(Res.getString("title.broadcast.history"));
-           
+
         try {
             readFromFile(myDate);
         } catch (IOException ex) {
             Log.error("Couldn't read from file", ex);
         }
-       
-        SearchButton.setText((Res.getString("button.search")));
-        SearchButton.addMouseListener(new java.awt.event.MouseAdapter() {
+
+        searchButton.setText((Res.getString("button.search")));
+        searchButton.addMouseListener(new java.awt.event.MouseAdapter() {
             @Override
-			public void mouseClicked(java.awt.event.MouseEvent evt) {
-                SearchButtonMouseClicked(evt);
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                searchButtonMouseClicked(evt);
             }
         });
-        
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+
+        GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(panelPane, javax.swing.GroupLayout.DEFAULT_SIZE, 533, javax.swing.GroupLayout.DEFAULT_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 20, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                	.addComponent(SearchDate)
-                    .addComponent(SearchButton)
-                    .addComponent(DateField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                   .addGap(22, 22, 22))
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(panelPane, GroupLayout.DEFAULT_SIZE, 533, GroupLayout.DEFAULT_SIZE)
+                    .addPreferredGap(ComponentPlacement.RELATED, 20, Short.MAX_VALUE)
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addComponent(searchDate)
+                        .addComponent(searchButton)
+                        .addComponent(dateField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                    .addGap(22, 22, 22))
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(15, 15, 15)
-                        .addComponent(panelPane, javax.swing.GroupLayout.DEFAULT_SIZE, 350, javax.swing.GroupLayout.DEFAULT_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(15, 15, 15)
-                        .addComponent(SearchDate)
-                        .addGap(10, 10, 10)
-                        .addComponent(DateField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(10, 10, 10)
-                        .addComponent(SearchButton)))
-                .addContainerGap(15, Short.MAX_VALUE))
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(15, 15, 15)
+                            .addComponent(panelPane, GroupLayout.DEFAULT_SIZE, 350, GroupLayout.DEFAULT_SIZE))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(15, 15, 15)
+                            .addComponent(searchDate)
+                            .addGap(10, 10, 10)
+                            .addComponent(dateField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                            .addGap(10, 10, 10)
+                            .addComponent(searchButton)))
+                    .addContainerGap(15, Short.MAX_VALUE))
         );
-  
+
         pack();
 
-        final Rectangle bounds = LayoutSettingsManager.getLayoutSettings().getBroadcastHistoryBounds();
-        if ( bounds == null || bounds.width <= 0 || bounds.height <= 0 )
-        {
+        Rectangle bounds = LayoutSettingsManager.getLayoutSettings().getBroadcastHistoryBounds();
+        if (bounds == null || bounds.width <= 0 || bounds.height <= 0) {
             // Use default settings.
-            setLocationRelativeTo( null );
-        }
-        else
-        {
-            setBounds( bounds );
+            setLocationRelativeTo(null);
+        } else {
+            setBounds(bounds);
         }
 
-        addComponentListener( new ComponentAdapter()
-        {
+        addComponentListener(new ComponentAdapter() {
             @Override
-            public void componentResized( ComponentEvent e )
-            {
-                LayoutSettingsManager.getLayoutSettings().setBroadcastHistoryBounds( getBounds() );
+            public void componentResized(ComponentEvent e) {
+                LayoutSettingsManager.getLayoutSettings().setBroadcastHistoryBounds(getBounds());
             }
 
             @Override
-            public void componentMoved( ComponentEvent e )
-            {
-                LayoutSettingsManager.getLayoutSettings().setBroadcastHistoryBounds( getBounds() );
+            public void componentMoved(ComponentEvent e) {
+                LayoutSettingsManager.getLayoutSettings().setBroadcastHistoryBounds(getBounds());
             }
-        } );
+        });
     }
 
 
-    private void SearchButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_SearchButtonMouseClicked
-        BroadcastHistoryArea.setText("");
+    private void searchButtonMouseClicked(java.awt.event.MouseEvent evt) {
+        broadcastHistoryArea.setText("");
         try {
-            readFromFile(DateField.getText());
+            readFromFile(dateField.getText());
         } catch (IOException ex) {
             Log.error("Couldn't read from file", ex);
         }
-    }//GEN-LAST:event_SearchButtonMouseClicked
+    }
 
     public void run() {
-    java.awt.EventQueue.invokeLater(() -> {
-       BroadcastHistoryFrame frame= new BroadcastHistoryFrame();
-       frame.setVisible(true);
-       frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    });
-}
+        invokeLater(() -> {
+            BroadcastHistoryFrame frame = new BroadcastHistoryFrame();
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        });
+    }
 
-   private javax.swing.JFormattedTextField DateField;
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private final javax.swing.JTextArea  BroadcastHistoryArea;
-    private javax.swing.JLabel SearchDate;
-    private javax.swing.JToggleButton SearchButton;
-    // End of variables declaration//GEN-END:variables
 }
