169b9bba423d786f9a59e6c0fe05f3cce609a93e
Sergey Ponomarev <stokito@gmail.com>
2026-07-17 20:22:08 +0300

    GeneralLoginSettingsPanel: use JSpinner for port and timeout inputs

--- core/src/main/java/org/jivesoftware/spark/ui/login/GeneralLoginSettingsPanel.java
+++ core/src/main/java/org/jivesoftware/spark/ui/login/GeneralLoginSettingsPanel.java
@@ -2,11 +2,9 @@ package org.jivesoftware.spark.ui.login;
 
 import org.jivesoftware.resource.Default;
 import org.jivesoftware.resource.Res;
-import org.jivesoftware.spark.util.ModelUtil;
 import org.jivesoftware.spark.util.ResourceUtils;
 import org.jivesoftware.sparkimpl.settings.JiveInfo;
 import org.jivesoftware.sparkimpl.settings.local.LocalPreferences;
-import org.jivesoftware.sparkimpl.settings.local.SettingsManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -16,6 +14,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import static java.awt.GridBagConstraints.*;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 /**
  * Internal class to set General settings
@@ -26,9 +25,9 @@ class GeneralLoginSettingsPanel extends JPanel implements ActionListener
     private final LocalPreferences localPreferences;
     private final JDialog optionsDialog;
     private final JCheckBox autoDiscoverBox = new JCheckBox();
-    private final JTextField portField = new JTextField();
+    private final JSpinner portField = new JSpinner(new SpinnerNumberModel(5222, 9, 65535, 1));
     private final JTextField xmppHostField = new JTextField();
-    private final JTextField timeOutField = new JTextField();
+    private final JSpinner timeOutField = new JSpinner(new SpinnerNumberModel(60, 5, Integer.MAX_VALUE, 5));
     private final JTextField resourceField = new JTextField();
     private final JCheckBox useHostnameAsResourceBox = new JCheckBox();
     private final JCheckBox useVersionAsResourceBox = new JCheckBox();
@@ -54,8 +53,8 @@ class GeneralLoginSettingsPanel extends JPanel implements ActionListener
         ResourceUtils.resButton( useVersionAsResourceBox, Res.getString( "checkbox.use.version.as.resource" ) );
         ResourceUtils.resButton( compressionBox, Res.getString( "checkbox.use.compression" ) );
         ResourceUtils.resButton( debuggerBox, Res.getString( "checkbox.use.debugger.on.startup" ) );
-        portField.setText( Integer.toString( localPreferences.getXmppPort() ) );
-        timeOutField.setText( Integer.toString( localPreferences.getTimeOut() ) );
+        portField.setValue( localPreferences.getXmppPort());
+        timeOutField.setValue(localPreferences.getTimeOut());
         autoLoginBox.setSelected( localPreferences.isAutoLogin() );
         xmppHostField.setText( localPreferences.getXmppHost() );
         resourceField.setText( localPreferences.getResource() );
@@ -67,16 +66,13 @@ class GeneralLoginSettingsPanel extends JPanel implements ActionListener
         useVersionAsResourceBox.addActionListener( this );
         useVersionAsResourceBox.setSelected( localPreferences.isUseVersionAsResource() );
         updateResourceVersion();
-
         resourceField.setEnabled(!(useHostnameAsResourceBox.isSelected() || useVersionAsResourceBox.isSelected()));
 
         autoDiscoverBox.addActionListener( this );
-
         autoDiscoverBox.setSelected( !localPreferences.isHostAndPortConfigured() );
         updateAutoDiscovery();
 
         compressionBox.setSelected( localPreferences.isCompressionEnabled() );
-
         debuggerBox.setSelected( localPreferences.isDebuggerEnabled() );
 
         final JPanel connectionPanel = new JPanel();
@@ -85,26 +81,25 @@ class GeneralLoginSettingsPanel extends JPanel implements ActionListener
 
         setLayout( new GridBagLayout() );
 
-        add( autoDiscoverBox, new GridBagConstraints( 0, 0, 2, 1, 1.0, 0.0, WEST, NONE, DEFAULT_INSETS, 0, 0 ) );
+        add(autoDiscoverBox, new GridBagConstraints(0, 0, 2, 1, 1, 0, WEST, NONE, DEFAULT_INSETS, 0, 0));
 
-        connectionPanel.add( xmppHostLabel, new GridBagConstraints( 0, 0, 1, 1, 0.0, 0.0, WEST, HORIZONTAL, DEFAULT_INSETS, 0, 0 ) );
-        connectionPanel.add( xmppHostField, new GridBagConstraints( 1, 0, 1, 1, 1.0, 0.0, WEST, NONE, DEFAULT_INSETS, 200, 0 ) );
-        connectionPanel.add( portLabel,     new GridBagConstraints( 0, 1, 1, 1, 0.0, 0.0, WEST, NONE, DEFAULT_INSETS, 0, 0 ) );
-        connectionPanel.add( portField,     new GridBagConstraints( 1, 1, 1, 1, 1.0, 0.0, WEST, NONE, DEFAULT_INSETS, 50, 0 ) );
+        connectionPanel.add(xmppHostLabel, new GridBagConstraints(0, 0, 1, 1, 0, 0, WEST, HORIZONTAL, DEFAULT_INSETS, 0, 0));
+        connectionPanel.add(xmppHostField, new GridBagConstraints(1, 0, 1, 1, 1, 0, WEST, NONE, DEFAULT_INSETS, 200, 0));
+        connectionPanel.add(portLabel, new GridBagConstraints(0, 1, 1, 1, 0, 0, WEST, NONE, DEFAULT_INSETS, 0, 0));
+        connectionPanel.add(portField, new GridBagConstraints(1, 1, 1, 1, 1, 0, WEST, NONE, DEFAULT_INSETS, 50, 0));
 
-        add( connectionPanel, new GridBagConstraints( 0, 1, 3, 1, 1.0, 0.0, NORTHWEST, BOTH, DEFAULT_INSETS, 0, 0 ) );
+        add(connectionPanel, new GridBagConstraints(0, 1, 3, 1, 1, 0, NORTHWEST, BOTH, DEFAULT_INSETS, 0, 0));
 
-        if ( Default.getBoolean( Default.USE_HOSTNAME_AS_RESOURCE ) == Default.getBoolean( Default.USE_VERSION_AS_RESOURCE ) )
-        {
-            add( resourceLabel,            new GridBagConstraints( 0, 2, 1, 1, 0.0, 0.0, WEST, NONE, DEFAULT_INSETS, 0, 0 ) );
-            add( resourceField,            new GridBagConstraints( 1, 2, 1, 1, 0.0, 0.0, WEST, NONE, DEFAULT_INSETS, 100, 0 ) );
-            add( useHostnameAsResourceBox, new GridBagConstraints( 0, 3, 2, 1, 0.0, 0.0, WEST, NONE, DEFAULT_INSETS, 0, 0 ) );
-            add( useVersionAsResourceBox,  new GridBagConstraints( 0, 4, 2, 1, 0.0, 0.0, WEST, NONE, DEFAULT_INSETS, 0, 0 ) );
+        if (Default.getBoolean(Default.USE_HOSTNAME_AS_RESOURCE) == Default.getBoolean(Default.USE_VERSION_AS_RESOURCE)) {
+            add(resourceLabel, new GridBagConstraints(0, 2, 1, 1, 0, 0, WEST, NONE, DEFAULT_INSETS, 0, 0));
+            add(resourceField, new GridBagConstraints(1, 2, 1, 1, 0, 0, WEST, NONE, DEFAULT_INSETS, 100, 0));
+            add(useHostnameAsResourceBox, new GridBagConstraints(0, 3, 2, 1, 0, 0, WEST, NONE, DEFAULT_INSETS, 0, 0));
+            add(useVersionAsResourceBox, new GridBagConstraints(0, 4, 2, 1, 0, 0, WEST, NONE, DEFAULT_INSETS, 0, 0));
         }
-        add( timeOutLabel,             new GridBagConstraints( 0, 5, 1, 1, 0.0, 0.0, WEST, NONE, DEFAULT_INSETS, 0, 0 ) );
-        add( timeOutField,             new GridBagConstraints( 1, 5, 1, 1, 0.0, 0.0, WEST, NONE, DEFAULT_INSETS, 50, 0 ) );
-        add( compressionBox,           new GridBagConstraints( 0, 6, 2, 1, 0.0, 0.0, WEST, HORIZONTAL, DEFAULT_INSETS, 0, 0 ) );
-        add( debuggerBox,              new GridBagConstraints( 0, 7, 2, 1, 0.0, 1.0, NORTHWEST, HORIZONTAL, DEFAULT_INSETS, 0, 0 ) );
+        add(timeOutLabel, new GridBagConstraints(0, 5, 1, 1, 0, 0, WEST, NONE, DEFAULT_INSETS, 0, 0));
+        add(timeOutField, new GridBagConstraints(1, 5, 1, 1, 0, 0, WEST, NONE, DEFAULT_INSETS, 50, 0));
+        add(compressionBox, new GridBagConstraints(0, 6, 2, 1, 0, 0, WEST, HORIZONTAL, DEFAULT_INSETS, 0, 0));
+        add(debuggerBox, new GridBagConstraints(0, 7, 2, 1, 0, 1, NORTHWEST, HORIZONTAL, DEFAULT_INSETS, 0, 0));
     }
 
     /**
@@ -176,42 +171,9 @@ class GeneralLoginSettingsPanel extends JPanel implements ActionListener
 
     public boolean validate_settings()
     {
-        String timeOut = timeOutField.getText();
-        String port = portField.getText();
         String resource = resourceField.getText();
-
         boolean valid = true;
-
-        try
-        {
-            Integer.valueOf( timeOut );
-        }
-        catch ( NumberFormatException numberFormatException )
-        {
-            JOptionPane.showMessageDialog( optionsDialog,
-                            Res.getString( "message.supply.valid.timeout" ),
-                            Res.getString( "title.error" ),
-                            JOptionPane.ERROR_MESSAGE );
-            timeOutField.requestFocus();
-            valid = false;
-        }
-
-        try
-        {
-            Integer.valueOf( port );
-        }
-        catch ( NumberFormatException numberFormatException )
-        {
-            JOptionPane.showMessageDialog( optionsDialog,
-                            Res.getString( "message.supply.valid.port" ),
-                            Res.getString( "title.error" ),
-                            JOptionPane.ERROR_MESSAGE );
-            portField.requestFocus();
-            valid = false;
-        }
-
-        if ( !ModelUtil.hasLength( resource ) )
-        {
+        if (isBlank(resource)) {
             JOptionPane.showMessageDialog( optionsDialog,
                             Res.getString( "message.supply.resource" ),
                             Res.getString( "title.error" ),
@@ -219,7 +181,6 @@ class GeneralLoginSettingsPanel extends JPanel implements ActionListener
             resourceField.requestFocus();
             valid = false;
         }
-
         return valid;
     }
 
@@ -229,15 +190,15 @@ class GeneralLoginSettingsPanel extends JPanel implements ActionListener
         useVersionAsResourceBox.setSelected(Default.getBoolean(Default.USE_VERSION_AS_RESOURCE));
         compressionBox.setSelected(Default.getBoolean(Default.COMPRESSION_ENABLED));
         debuggerBox.setSelected(Default.getBoolean(Default.DEBUGGER_ENABLED));
-        portField.setText(Default.getString(Default.XMPP_PORT));
+        portField.setValue(Default.getString(Default.XMPP_PORT));
         resourceField.setText(Default.getString(Default.SHORT_NAME));
-        timeOutField.setText(Default.getString(Default.TIME_OUT));
+        timeOutField.setValue(Default.getString(Default.TIME_OUT));
     }
     
     public void saveSettings()
     {
-        localPreferences.setTimeOut( Integer.parseInt( timeOutField.getText() ) );
-        localPreferences.setXmppPort( Integer.parseInt( portField.getText() ) );
+        localPreferences.setTimeOut((int) timeOutField.getValue());
+        localPreferences.setXmppPort((int) portField.getValue());
         localPreferences.setXmppHost( xmppHostField.getText() );
         localPreferences.setCompressionEnabled( compressionBox.isSelected() );
         localPreferences.setDebuggerEnabled( debuggerBox.isSelected() );
