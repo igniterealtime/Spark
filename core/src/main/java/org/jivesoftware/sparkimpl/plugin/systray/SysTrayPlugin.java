1770cb7810b60624c630b1fe5f288ecfa83d0435
Sergey Ponomarev <stokito@gmail.com>
2026-07-20 23:12:10 +0300

    SysTrayPlugin: use the same 24px icons on Windows too

--- core/src/main/java/org/jivesoftware/sparkimpl/plugin/systray/SysTrayPlugin.java
+++ core/src/main/java/org/jivesoftware/sparkimpl/plugin/systray/SysTrayPlugin.java
@@ -103,34 +103,18 @@ public class SysTrayPlugin implements Plugin, NativeHandler, ChatStateListener {
         //XEP-0085 support (replaces the obsolete XEP-0022)
         ChatStateManager.getInstance(SparkManager.getConnection()).addChatStateListener(this);
 
-        if (Spark.isLinux()) {
-            newMessageIcon = SparkRes.getImageIcon(SparkRes.Icon.MESSAGE_NEW_TRAY_LINUX);
-            typingIcon = SparkRes.getImageIcon(SparkRes.Icon.TYPING_TRAY_LINUX);
-        } else {
-            newMessageIcon = SparkRes.getImageIcon(SparkRes.Icon.MESSAGE_NEW_TRAY);
-            typingIcon = SparkRes.getImageIcon(SparkRes.Icon.TYPING_TRAY);
-        }
+        newMessageIcon = SparkRes.getImageIcon(SparkRes.Icon.MESSAGE_NEW_TRAY);
+        typingIcon = SparkRes.getImageIcon(SparkRes.Icon.TYPING_TRAY);
 
         availableIcon = Default.getImageIcon(Default.TRAY_IMAGE);
-        if (Spark.isLinux()) {
-            if (availableIcon == null) {
-                availableIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_IMAGE_LINUX);
-            }
-            awayIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_AWAY_LINUX);
-            xawayIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_XAWAY_LINUX);
-            dndIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_DND_LINUX);
-            offlineIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_OFFLINE_LINUX);
-            connectingIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_CONNECTING_LINUX);
-        } else {
-            if (availableIcon == null) {
-                availableIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_IMAGE);
-            }
-            awayIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_AWAY);
-            xawayIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_XAWAY);
-            dndIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_DND);
-            offlineIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_OFFLINE);
-            connectingIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_CONNECTING);
+        if (availableIcon == null) {
+            availableIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_IMAGE);
         }
+        awayIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_AWAY);
+        xawayIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_XAWAY);
+        dndIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_DND);
+        offlineIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_OFFLINE);
+        connectingIcon = SparkRes.getImageIcon(SparkRes.Icon.TRAY_CONNECTING);
 
         popupMenu.add(openMenu);
 
@@ -139,7 +123,6 @@ public class SysTrayPlugin implements Plugin, NativeHandler, ChatStateListener {
             public void actionPerformed(ActionEvent event) {
                 SparkManager.getMainWindow().bringFrameIntoFocus();
             }
-
         });
 
         popupMenu.add(minimizeMenu);
